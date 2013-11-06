cf760846aaf8941f995a6c8d48ab684f9d85e033
Shlomi Gridish <gridish@freescale.com>
2013-11-06 08:39:23 +0200

    clean ups

--- src/kernel/cmdif_client.c
+++ src/kernel/cmdif_client.c
@@ -6,6 +6,47 @@
 #include "dplib/fsl_mc.h"
 
 
+#define MC_CMD_OPEN_SIZE	8
+#define MC_CMD_CLOSE_SIZE	0
+
+/**************************************************************************//**
+ @Collection    Command portal Header
+ @{
+ *//***************************************************************************/
+#define MC_CMD_CMDID_OFFSET	52	/**< Command offset */
+#define MC_CMD_CMDID_SIZE	12	/**< command size */
+#define MC_CMD_AUTHID_OFFSET	38	/**< Authentication offset */
+#define MC_CMD_AUTHID_SIZE	10	/**< Authentication size */
+#define MC_CMD_SIZE_OFFSET	32	/**< Size offset */
+#define MC_CMD_SIZE_SIZE	6	/**< Size of size */
+#define MC_CMD_STATUS_OFFSET	16	/**< Status offset */
+#define MC_CMD_STATUS_SIZE	8	/**< Status size*/
+#define MC_CMD_PRI_OFFSET	15	/**< Priority offset */
+#define MC_CMD_PRI_SIZE		1	/**< Priority size */
+
+
+#define MC_CMD_DPNI_OPEN	0x201
+#define MC_CMD_DPSW_OPEN	0x202
+#define MC_CMD_DPIO_OPEN	0x203
+#define MC_CMD_DPSP_OPEN	0x204
+#define MC_CMD_DPRC_OPEN	0x205
+
+#define MC_CMD_CLOSE		0x200
+
+/* @} */
+
+
+struct mc_portal_regs {
+	uint64_t header;
+	uint64_t param1;
+	uint64_t param2;
+	uint64_t param3;
+	uint64_t param4;
+	uint64_t param5;
+	uint64_t param6;
+	uint64_t param7;
+};
+
 struct cmdif_dev {
 	struct mc_portal_regs *regs;
 	int auth_id;
