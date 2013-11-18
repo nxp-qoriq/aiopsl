9be8ff55485c7451487cdea7b91145e0d31bc7bb
Shlomi Gridish <gridish@freescale.com>
2013-11-18 06:24:34 +0200

    cosmetics

--- src/drivers/dplib/dpni/dpni_drv.c
+++ src/drivers/dplib/dpni/dpni_drv.c
@@ -3,6 +3,10 @@
 
 #define __ERR_MODULE__  MODULE_DPNI
 
+#define DPNI_DRV_FLG_ENABLED    0x80
+#define DPNI_DRV_FLG_PARSE      0x40
+#define DPNI_DRV_FLG_GEN_CS     0x20
+
 
 void receive_cb (void);
 
@@ -11,8 +15,14 @@ void dpni_drv_free(void);
 
 
 struct dpni_drv {
-    
+    uint8_t         id;
+    uint8_t         flags;
+    uint16_t        spid;
+    uint16_t        qdid;
+    rx_cb_t         *rx_cbs[8];
+    fsl_handle_t    args[8];
 };
+
 
 void receive_cb (void)
 {
