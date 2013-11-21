9476b03334d7c347b6f113fc033f4682e63386f0
Yariv Sheffer <yariv.sheffer@freescale.com>
2013-11-21 09:39:15 +0200

    Fixes for compilation errors/warnings

--- src/drivers/dplib/dpni/dpni.h
+++ src/drivers/dplib/dpni/dpni.h
@@ -109,6 +109,8 @@ void prepare_clear_fs_tbl_cmd(struct cmdif_cmd_desc *desc, uint8_t tcid);
 void recieve_get_stats_cmd(struct cmdif_cmd_desc *desc, 
 						   struct dpni_stats *stats);
 
+int dpni_modify_primary_mac_addr(struct dpni *dpni, uint8_t addr[]);
+
 
 
 #endif /* _DPNI_H */
