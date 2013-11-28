3848fc0d2bda105c98555a27fa8a62f931f33f3d
Emil watralov <Emil.Watralov@freescale.com>
2013-11-28 13:05:03 +0200

    pragma unused removed

--- src/lib/time.c
+++ src/lib/time.c
@@ -9,18 +9,19 @@
 time_t time(
     time_t *t) 
 {
-	/* to be done */
-#pragma unused (t)
-	return 0;
+    /* to be done */
+    UNUSED (t);
+    return 0;
 }
  
 int gettimeofday(
     struct timeval *tv,
     struct timezone *tz)
 {
-	/* to be done */
-#pragma unused (tv, tz) 
-	return 0;
+    /* to be done */
+    UNUSED (tz);
+    UNUSED (tv);
+    return 0;
 }
 
 
