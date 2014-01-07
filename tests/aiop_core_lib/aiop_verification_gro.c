82a9e3087bd4dec8e378d1cd55518971c97dc8a3
Gal Biran <gal.biran@freescale.com>
2014-01-07 15:56:06 +0200

    Add GRO verification wrapper.

---
+++ tests/aiop_core_lib/aiop_verification_gro.c
@@ -0,0 +1,64 @@
+/**************************************************************************//**
+@File		aiop_verification_gro.c
+
+@Description	This file contains the AIOP GRO SW Verification Structures.
+
+		Copyright 2013 Freescale Semiconductor, Inc.
+*//***************************************************************************/
+
+
+#include "aiop_verification.h"
+
+extern __TASK struct aiop_default_task_params default_task_params;
+
+uint16_t  aiop_verification_gro(uint32_t data_addr)
+{
+	uint16_t str_size = STR_SIZE_ERR;
+	uint32_t opcode;
+	uint32_t flags;
+
+	opcode  = *((uint32_t *) data_addr);
+	flags = 0x0;
+
+	switch (opcode) {
+	case TCP_GRO_CONTEXT_AGG_SEG_CMD_STR:
+	{
+		struct tcp_gro_agg_seg_command *str =
+			(struct tcp_gro_agg_seg_command *)data_addr;
+		
+		str->status = tcp_gro_aggregate_seg(
+				str->tcp_gro_context_addr, 
+				&(str->params),str->flags);
+		
+		str_size = sizeof(struct tcp_gro_agg_seg_command);
+		break;
+	}
+	case TCP_GRO_CONTEXT_FLUSH_AGG_CMD_STR:
+	{
+		struct tcp_gro_flush_agg_command *str =
+			(struct tcp_gro_flush_agg_command *)data_addr;
+		
+		str->status = tcp_gro_flush_aggregation(
+				str->tcp_gro_context_addr);
+			/* The aggregated packet that was generated is now the 
+			 * default frame of the task */
+		
+		str_size = sizeof(struct tcp_gro_flush_agg_command);
+		break;
+	}
+	default:
+	{
+		return STR_SIZE_ERR;
+	}
+	}
+
+	return str_size;
+}
+
+
+
+
+
+
+
+
