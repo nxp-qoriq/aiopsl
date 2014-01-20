2b22d4e5b2ea5e9d64d7b17b0f03f546855e80ae
Amir Yosha <amiryo@freescale.com>
2014-01-20 10:58:22 +0200

    Working on tcp_gro_close_aggregation_and_open_new_aggregation() GRO function.

--- src/drivers/dplib/netf/l4/gro.c
+++ src/drivers/dplib/netf/l4/gro.c
@@ -423,9 +423,149 @@ int32_t tcp_gro_close_aggregation_and_open_new_aggregation(
 		struct tcp_gro_context_params *params,
 		struct tcp_gro_context *gro_ctx)
 {
-	/* Todo - return valid status */
-	return (int32_t)gro_ctx + (int32_t)params + 
-			(int32_t)tcp_gro_context_addr;
+	struct tcphdr *tcp;
+	uint8_t data_offset;
+	uint16_t headers_size, outer_ip_offset, ip_length;
+	struct ipv4hdr *ipv4;
+	struct ipv6hdr *ipv6;
+	struct ldpaa_fd tmp_fd;
+	int32_t sr_status;
+				
+	tcp = (struct tcphdr *)(PARSER_GET_L4_POINTER_DEFAULT());
+			
+	/* initialize gro_context parameters */	
+	gro_ctx->internal_flags = 0;
+	gro_ctx->last_ack = tcp->acknowledgment_number;
+	data_offset = tcp->data_offset_reserved >> 
+				NET_HDR_FLD_TCP_DATA_OFFSET_OFFSET;
+	headers_size = (uint16_t)(PARSER_GET_L4_OFFSET_DEFAULT() + data_offset);
+	gro_ctx->next_seq = tcp->acknowledgment_number + 
+			(uint16_t)LDPAA_FD_GET_LENGTH(HWC_FD_ADDRESS) - 
+			headers_size;
+	/* save timestap if exist */
+	if (data_offset > TCP_HDR_LENGTH){
+		gro_ctx->timestamp = ((struct tcphdr_gro *)tcp)->tsval;
+		gro_ctx->internal_flags |= TCP_GRO_HAS_TIMESTAMP; 
+	}
+		
+	/* Set ECN flags */
+	if (PARSER_IS_OUTER_IPV4_DEFAULT()) {
+		/* IPv4 */
+		ipv4 = (struct ipv4hdr *)PARSER_GET_OUTER_IP_POINTER_DEFAULT();
+		gro_ctx->internal_flags |= (*((uint32_t *)ipv4) & 
+				TCP_GRO_ECN_MASK);
+	}
+	else {
+		/* IPv6 */
+		ipv6 = (struct ipv6hdr *)PARSER_GET_OUTER_IP_POINTER_DEFAULT();
+		gro_ctx->internal_flags |= ((*((uint32_t *)ipv6)) >> 4 & 
+				TCP_GRO_ECN_MASK);
+	}
+		
+	if (tcp->flags & NET_HDR_FLD_TCP_FLAGS_PSH)
+		/* PSH flag set */
+		gro_ctx->internal_flags |= TCP_GRO_PSH_FLAG_SET;
+				
+	/* store segment frame */
+	sr_status = fdma_store_default_frame_data(); /* TODO FDMA ERROR */
+		
+	/* replace between default_FD and agg_FD (segment <--> agg frame) */
+	tmp_fd = *((struct ldpaa_fd *)HWC_FD_ADDRESS);
+	*((struct ldpaa_fd *)HWC_FD_ADDRESS) = gro_ctx->agg_fd;
+	gro_ctx->agg_fd = tmp_fd;
+		
+	/* present frame (default_FD(agg_FD)) +  present header */
+	sr_status = fdma_present_default_frame(); /* TODO FDMA ERROR */
+		
+	/* update last segment header fields */
+	*((struct tcp_gro_last_seg_header_fields *) 
+		(&(tcp->acknowledgment_number))) = gro_ctx->last_seg_fields;
+		
+	/* update statistics */
+	ste_inc_and_acc_counters(params->stats_addr + 
+			GRO_STAT_AGG_NUM_CNTR_OFFSET, 1, 
+			STE_MODE_COMPOUND_32_BIT_CNTR_SIZE | 
+			STE_MODE_COMPOUND_32_BIT_ACC_SIZE |
+			STE_MODE_COMPOUND_CNTR_SATURATE |
+			STE_MODE_COMPOUND_ACC_SATURATE);
+		
+	/* write metadata to external memory */
+	sr_status = cdma_write((params->metadata + 
+			METADATA_MEMBER1_SIZE), &(gro_ctx->metadata.seg_num), 
+			METADATA_MEMBER2_SIZE + METADATA_MEMBER3_SIZE);
+		
+	/* update IP length + checksum */
+	outer_ip_offset = (uint16_t)PARSER_GET_OUTER_IP_OFFSET_DEFAULT();
+	ip_length = (uint16_t)LDPAA_FD_GET_LENGTH(HWC_FD_ADDRESS) - 
+			outer_ip_offset;
+	if (PARSER_IS_OUTER_IPV4_DEFAULT()){
+		/* IPv4 */
+		ipv4 = (struct ipv4hdr *)PARSER_GET_OUTER_IP_POINTER_DEFAULT();
+		if (gro_ctx->flags & TCP_GRO_CALCULATE_IP_CHECKSUM)
+			cksum_update_uint32(&(ipv4->hdr_cksum),
+					ipv4->total_length, ip_length);
+			ipv4->total_length = ip_length;
+		}
+	else{
+		/* IPv6 */
+		ipv6 = (struct ipv6hdr *)PARSER_GET_OUTER_IP_POINTER_DEFAULT();
+		ipv6->payload_length = ip_length - IPV6_HDR_LENGTH;
+	}
+		
+	/* update TCP checksum */
+	if (gro_ctx->flags & TCP_GRO_CALCULATE_TCP_CHECKSUM)
+			tcp_gro_calc_tcp_header_cksum(gro_ctx);
+		
+	/* Save headers changes to FDMA */
+	sr_status = fdma_modify_default_segment_data(outer_ip_offset, (uint16_t)
+			(PARSER_GET_L4_OFFSET_DEFAULT() + 
+					TCP_HDR_LENGTH - outer_ip_offset));
+	/* calculate tcp data checksum */
+	if (gro_ctx->flags & TCP_GRO_CALCULATE_TCP_CHECKSUM)
+			gro_ctx->checksum = tcp_gro_calc_tcp_data_cksum(); // ?????????
+			
+	if (gro_ctx->internal_flags & TCP_GRO_PSH_FLAG_SET) {
+		/* create zero timer for the new PUSH segment */
+		sr_status = tman_create_timer(params->timeout_params.tmi_id,
+				gro_global_params.timeout_flags, 
+				0, 
+				tcp_gro_context_addr, 
+				(uint16_t)(uint32_t)&tcp_gro_timeout_callback, 
+				gro_global_params.gro_timeout_epid,
+				0, 
+				&(gro_ctx->timer_handle));
+	}
+	else {
+		/* create timer for the new aggregation */
+		sr_status = tman_create_timer(params->timeout_params.tmi_id, 
+				gro_global_params.timeout_flags,
+				params->limits.timeout_limit, 
+				tcp_gro_context_addr, 
+				(uint16_t)(uint32_t)&tcp_gro_timeout_callback, 
+				gro_global_params.gro_timeout_epid,
+				0, 
+				&(gro_ctx->timer_handle));
+		/* recharge timer for the new aggregation */
+		/* sr_status = tman_recharge_timer(gro_ctx->timer_handle); */
+	}
+		
+	/* initialize gro context fields */
+	gro_ctx->params = *params;
+	//gro_ctx->flags = gro_ctx.flags;   // ??????????????
+	gro_ctx->metadata.seg_num = 1;
+	gro_ctx->metadata.max_seg_size = 
+			(uint16_t)LDPAA_FD_GET_LENGTH(HWC_FD_ADDRESS);
+	
+	/* update seg size */
+	if(gro_ctx->flags & TCP_GRO_METADATA_SEGMENT_SIZES) {
+		sr_status = cdma_read(&(gro_ctx->metadata.seg_sizes_addr), 
+				params->metadata , METADATA_MEMBER1_SIZE);
+		sr_status = cdma_write(gro_ctx->metadata.seg_sizes_addr, 
+				&(gro_ctx->metadata.max_seg_size), 
+				sizeof(gro_ctx->metadata.max_seg_size));	
+	}
+	
+	return TCP_GRO_SEG_AGG_DONE_AGG_OPEN_NEW_AGG;	
 }
 
 int32_t tcp_gro_flush_aggregation(
