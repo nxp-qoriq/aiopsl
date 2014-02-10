4a8b84311ef706a12b7687c0b5ccca233ce48f6c
Hagit Gilboa <hagit.gilboa@freescale.com>
2014-02-10 08:30:50 +0200

    Removed: struct ipf_mtu_params, union ipf_mtu_sf_params. Changed struct ipf_context

--- src/drivers/dplib/netf/ip/ipf.h
+++ src/drivers/dplib/netf/ip/ipf.h
@@ -26,38 +26,11 @@
 *//***************************************************************************/
 
 /**************************************************************************//**
-@Description	IPF parameters used by IPF functions for split by MTU.
-*//***************************************************************************/
-struct ipf_mtu_params {
-	/** Maximum Transmit Unit
-	 * In case \ref IPF_RESTORE_ORIGINAL_FRAGMENTS flag is set, this
-	 * parameter is ignored. */
-	uint16_t mtu;
-	/** MTU payload length */
-	uint16_t mtu_payload_length;
-	/** Split size */
-	uint16_t split_size;
-	/** Previous Fragment Offset */
-	uint16_t prev_frag_offset;
-};
-
-/**************************************************************************//**
-@Description	IPF union parameters used by IPF functions.
-*//***************************************************************************/
-union ipf_mtu_sf_params {
-	struct ipf_mtu_params mtu_params;
-	uint16_t frag_header_offset[4];
-};
-
-
-/**************************************************************************//**
 @Description	IPF Context Internally used by IPF functions.
 *//***************************************************************************/
 struct ipf_context {
 	 /** Remaining frame's FD  */
 	struct ldpaa_fd rem_fd;
-	/** Union of ipf_mtu_params and ipf_sf_params */
-	union ipf_mtu_sf_params ipf_params ;
 	/** Frame ID. Used for IPv6 fragmentation extension in case of
 	 * fragmentation according to MTU.
 	 * In case \ref IPF_RESTORE_ORIGINAL_FRAGMENTS flag is set, this
@@ -66,24 +39,36 @@ struct ipf_context {
 	uint32_t frame_id;
 	/** Flags - Please refer to \ref IPF_Flags */
 	uint32_t flags;
-	/** Fragment index (for SF mode) */
-	uint16_t frag_index;
 	/** Original Starting HXS for Parser from Task default */
 	uint16_t parser_starting_hxs;
 	/** Original segment address from PRC */
 	uint16_t prc_seg_address;
 	/** Original segment length from PRC */
 	uint16_t prc_seg_length;
-	/** Remaining payload length */
+	/** Remaining payload length (for split by MTU) */
 	uint16_t remaining_payload_length;
+	/** Maximum Transmit Unit
+	 * In case \ref IPF_RESTORE_ORIGINAL_FRAGMENTS flag is set, this
+	 * parameter is ignored. */
+	uint16_t mtu;
+	/** MTU payload length (for split by MTU) */
+	uint16_t mtu_payload_length;
+	/** Split size (for split by MTU) */
+	uint16_t split_size;
+	/** Previous Fragment Offset */
+	uint16_t prev_frag_offset;
 	/** Original Parser Profile ID from Task default */
 	uint8_t parser_profile_id;
-	/** Remaining frame handle*/
+	/** Remaining frame handle */
 	uint8_t rem_frame_handle;
 	/** First fragment indication */
 	uint8_t	first_frag;
 	/** IPv4 indication */
 	uint8_t ipv4;
+	/** IP offset */
+	uint8_t ip_offset;
+	/** IPv6 Fragment header offset	 */ 
+	uint8_t ipv6_frag_hdr_offset;
 	/* Padding */
 	uint8_t	pad[2];
 };
