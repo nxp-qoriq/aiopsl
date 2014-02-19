ef5ba28c72bf1632f2f1c864b62a7e1e633a9e89
Ira Kalman <Ira.Kalman@freescale.com>
2014-02-19 17:17:55 +0200

    Added peb & ddr SLAB test

--- build/aiop_t4ls_sim/apps/app_process_packet/src/app_process_packet.c
+++ build/aiop_t4ls_sim/apps/app_process_packet/src/app_process_packet.c
@@ -19,40 +19,81 @@ void app_free(void);
 #define APP_FLOW_GET(ARG) (((uint16_t)(((ARG) & 0xFFFF0000) >> 16) 
 /**< Get flow id from callback argument, it's demo specific macro */
 
-uint32_t   slab = 0;
+uint32_t  slab_peb = 0;
+uint32_t  slab_ddr = 0;
+
+static int app_write_buff_and_release(uint32_t slab, uint64_t buff)
+{
+    uint64_t data1 = 0xAABBCCDD00001122;
+    uint64_t data2 = 0;
+    int      err = 0;
+
+    err = cdma_write(buff, &data1, 8); 
+    if (err) return err;
+    err = cdma_read(&data2, buff, 8);
+    if (err) return err;
+    
+    if (data1 != data2) {
+        slab_release(slab, buff);
+        return -EPERM;              
+    }
+
+    err = slab_release(slab, buff); 
+    if (err) return err;
+    
+    return 0;
+}
+
+static int app_test_slab(uint32_t slab, int num_times) 
+{
+    uint64_t buff[3] = {0, 0, 0};
+    int      err = 0;
+    int      i = 0;
+    
+    for (i = 0; i < num_times; i++) {
+        
+        err = slab_acquire(slab, &buff[0]);
+        if (err || (buff == NULL)) return -ENOMEM;
+        err = slab_acquire(slab, &buff[1]);
+        if (err || (buff == NULL)) return -ENOMEM;
+        err = slab_acquire(slab, &buff[2]);
+        if (err || (buff == NULL)) return -ENOMEM;
+
+        err = app_write_buff_and_release(slab, buff[1]);
+        if (err) return err;
+        err = app_write_buff_and_release(slab, buff[2]);
+        if (err) return err;
+        err = app_write_buff_and_release(slab, buff[0]);
+        if (err) return err;
+    }
+    
+    return 0;
+}
 
 static void app_process_packet_flow0 (dpni_drv_app_arg_t arg)
 {
-	uint32_t src_addr = 0x10203040;// new ipv4 src_addr
-	int32_t ret = SUCCESS;
-	ret = ip_set_nw_src(src_addr);
-	if (ret != SUCCESS)
-		fsl_os_print("AIOP test: Error while replacing header src address\n");
-	dpni_drv_send(APP_NI_GET(arg));
+	int      err = 0;
+    uint32_t src_addr = 0x10203040;// new ipv4 src_addr
+
+    err = ip_set_nw_src(src_addr);
+    if (err) fdma_terminate_task();
+	
+    err = app_test_slab(slab_peb, 4);
+    if (err) fdma_terminate_task();
+
+    dpni_drv_send(APP_NI_GET(arg));
 	fdma_terminate_task();
 }
 
 static void app_process_packet_flow1 (dpni_drv_app_arg_t arg)
 {    
-    uint64_t buff = 0;
-    uint64_t data1 = 0xAABBCCDD;
-    uint64_t data2 = 0;
-    int err       = 0;
+    int      err = 0;
     
-    if (slab_acquire(slab, &buff)) {
-        fdma_terminate_task();
-    } 
-    
-    err = cdma_write(buff, &data1, 8);        
-    err = cdma_read(&data2, buff, 8);
-    if (data1 != data2) {
-        slab_release(slab, buff);
-        fdma_terminate_task();              
-    } else {
-        slab_release(slab, buff);        
-    }
+    err = app_test_slab(slab_ddr, 4);
+    if (err) fdma_terminate_task();
 
-    app_process_packet_flow0 (arg);
+    dpni_drv_send(APP_NI_GET(arg));
+    fdma_terminate_task();
 }
 
 int app_init(void)
@@ -83,24 +124,32 @@ int app_init(void)
         if (err) return err;
     }
     
-    err = slab_create(5, 0, 100, 0, 0, 4, MEM_PART_PEB, 0, NULL, &slab);
+    /* DDR SLAB can be created only at init */
+    err = slab_create(10, 0, 256, 0, 0, 4, MEM_PART_1ST_DDR_NON_CACHEABLE, 0, NULL, &slab_ddr);
     if (err) return err;
-    fsl_os_print("--------------------slab_create() passed --------------------\n");
 
-    err = slab_acquire(slab, &buff);
+    err = slab_acquire(slab_ddr, &buff);
     if (err) return err;
-    fsl_os_print("--------------------slab_acquire() passed --------------------\n");
 
-    err = slab_release(slab, buff);
+    err = slab_release(slab_ddr, buff);
     if (err) return err;
-    fsl_os_print("--------------------slab_release() passed --------------------\n");
-
+    
+    err = slab_create(10, 0, 256, 0, 0, 4, MEM_PART_PEB, 0, NULL, &slab_peb);
+    if (err) return err;        
+ 
     return err;
 }
 
 void app_free(void)
 {
+    int err = 0;
+    
     /* TODO - complete!*/
-    slab_free(slab);
-    fsl_os_print("--------------------slab_free() passed --------------------\n");
+    err = slab_free(slab_ddr);
+    if (err == 0) 
+        fsl_os_print("--------------------slab_free() passed --------------------\n");
+
+    err = slab_free(slab_peb);
+    if (err == 0) 
+        fsl_os_print("--------------------slab_free() passed --------------------\n");
 }
