9535886bb5120d251528ca766a9c2d4669f53d31
Hagit Gilboa <hagit.gilboa@freescale.com>
2014-03-30 14:33:01 +0300

    proeparations for implementation with slab_find_and_fill_bpid

--- src/drivers/dplib/arch/accel/system.c
+++ src/drivers/dplib/arch/accel/system.c
@@ -7,6 +7,10 @@
 *//***************************************************************************/
 #include "system.h"
 #include "id_pool.h"
+/* TODO include the followings when ARENA will be included
+#include "slab.h"
+#include "kernel/platform.h"
+*/
 
 /* Global parameters*/
 uint64_t ext_prpid_pool_address;
@@ -19,12 +23,22 @@ int32_t sys_ctlu_prpid_pool_create(void)
 
 	/* TODO Replace these temporal assignments with buffer_pool_id and
 	 * buffer_size provided by ARENA function*/
-	uint16_t buffer_pool_id = 1;
-/*	uint32_t buffer_size = 256;*/
+	uint16_t buffer_pool_id=1;
+	int num_filled_buffs;
+
+/*
+	status = slab_find_and_fill_bpid
+			(1, (SYS_NUM_OF_PRPIDS+1), 2, 
+			MEM_PART_1ST_DDR_NON_CACHEABLE,
+			&num_filled_buffs,&buffer_pool_id);
+	if (status < 0)
+		return status;
+*/
 
 	status = id_pool_init(SYS_NUM_OF_PRPIDS, buffer_pool_id,
 					&ext_prpid_pool_address);
-	return status;
+
+		return status;
 }
 
 
@@ -32,6 +46,16 @@ int32_t sys_ctlu_keyid_pool_create(void)
 {
 /*	uint16_t pool[SYS_KEYID_POOL_LENGTH];*/
 	int32_t status;
+	int num_filled_buffs;
+
+/*
+	status = slab_find_and_fill_bpid
+			(1, (SYS_NUM_OF_KEYIDS+1), 2, 
+			MEM_PART_1ST_DDR_NON_CACHEABLE,
+			&num_filled_buffs,&buffer_pool_id);
+	if (status < 0)
+		return status;
+*/
 
 	/* TODO Replace these temporal assignments with buffer_pool_id and
 	 * buffer_size provided by ARENA function*/
