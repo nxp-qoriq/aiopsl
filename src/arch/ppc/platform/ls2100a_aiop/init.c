e2fb36d309a1429b86d06ed9576e419da99b69c9
Ira Kalman <Ira.Kalman@freescale.com>
2014-04-24 15:06:40 +0300

    Fixed bug in SHRAM

--- src/arch/ppc/platform/ls2100a_aiop/init.c
+++ src/arch/ppc/platform/ls2100a_aiop/init.c
@@ -33,7 +33,7 @@ extern void build_apps_array(struct sys_module_desc *apps);
     {PLTFRM_MEM_RGN_MC_PORTALS, MEM_PART_INVALID,               0x80c000000LL, 0x08000000, (64  * MEGABYTE) },\
     {PLTFRM_MEM_RGN_AIOP,       MEM_PART_INVALID,               0x02000000,    0x02000000, (384 * KILOBYTE) },\
     {PLTFRM_MEM_RGN_CCSR,       MEM_PART_INVALID,               0x08000000,    0x0c000000, (1 * MEGABYTE)   },\
-    {PLTFRM_MEM_RGN_SHRAM,      MEM_PART_SH_RAM,                0x01000400,    0x01000400, (255 * KILOBYTE) },\
+    {PLTFRM_MEM_RGN_SHRAM,      MEM_PART_SH_RAM,                0x01010000,    0x01010000, (192 * KILOBYTE) },\
     {PLTFRM_MEM_RGN_DDR1,       MEM_PART_1ST_DDR_NON_CACHEABLE, 0x58000000,    0x58000000, (128 * MEGABYTE) },\
     {PLTFRM_MEM_RGN_PEB,        MEM_PART_PEB,                   0x80000000,    0x80000000, (2 * MEGABYTE)   },\
 }
