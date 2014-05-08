c594e815ca6f99cbb73f9b4803f3fe84b0d46f33
Hagit Gilboa <hagit.gilboa@freescale.com>
2014-05-08 15:26:31 +0300

    Removed  Profile Sram initialization (moved to aiop_sl_init in system.c)

--- tests/aiop_core_lib/aiop_verification_data.h
+++ tests/aiop_core_lib/aiop_verification_data.h
@@ -14,28 +14,31 @@
 
 #define __VERIF_GLOBAL __declspec(section ".verif_data")
 #define __VERIF_TLS __declspec(section ".verif_tdata")
-#define __VERIF_PROFILE_SRAM __declspec(section ".verif_psram_data")
+//#define __VERIF_PROFILE_SRAM __declspec(section ".verif_psram_data")
 
 #pragma section RW ".verif_data" ".verif_bss"
 #pragma section RW ".verif_tdata" ".verif_tbss"
-#pragma section RW ".verif_psram_data" ".verif_psram_bss"
+//#pragma section RW ".verif_psram_data" ".verif_psram_bss"
 
+
+/*
 struct profile_sram {
-	uint64_t ip_secific_sp_info; /**< IP-Specific SP Information 	 */
-	uint16_t dl; /**<  DataLength(correction)	 */
-	uint16_t reserved; /**< reserved 	 */
-	uint16_t dhr; /**< DataHeadRoom(correction) */
-	uint8_t  mode_bits1; /**< mode bits 	 */
-	uint8_t  mode_bits2; /**< mode bits 	 */
-	uint16_t pbs1; /**<  Pool Buffer Size	 */
-	uint16_t bpid1; /**<  Bypass Memory Translation	 */
-	uint16_t pbs2; /**<  Pool Buffer Size	 */
-	uint16_t bpid2; /**<  Bypass Memory Translation	 */
-	uint16_t pbs3; /**<  Pool Buffer Size	 */
-	uint16_t bpid3; /**<  Bypass Memory Translation	 */
-	uint16_t pbs4; /**<  Pool Buffer Size	 */
-	uint16_t bpid4; /**<  Bypass Memory Translation	 */
+	uint64_t ip_secific_sp_info; *< IP-Specific SP Information 	 
+	uint16_t dl; *<  DataLength(correction)	 
+	uint16_t reserved; *< reserved 	 
+	uint16_t dhr; *< DataHeadRoom(correction) 
+	uint8_t  mode_bits1; *< mode bits 	 
+	uint8_t  mode_bits2; *< mode bits 	 
+	uint16_t pbs1; *<  Pool Buffer Size	 
+	uint16_t bpid1; *<  Bypass Memory Translation	 
+	uint16_t pbs2; *<  Pool Buffer Size	 
+	uint16_t bpid2; *<  Bypass Memory Translation	 
+	uint16_t pbs3; *<  Pool Buffer Size	 
+	uint16_t bpid3; *<  Bypass Memory Translation	 
+	uint16_t pbs4; *<  Pool Buffer Size	 
+	uint16_t bpid4; *<  Bypass Memory Translation	 
 };
+*/
 
 //struct profile_sram {
 //	uint64_t ip_secific_sp_info; /**< IP-Specific SP Information 	 */
@@ -54,6 +57,7 @@ struct profile_sram {
 //	uint16_t pbs4; /**<  Pool Buffer Size	 */
 //};
 
+/*
 	#define mode_bits1_PTAR_MASK  0x80
 	#define mode_bits1_SGHR_MASK  0x10
 	#define mode_bits1_ASAR_MASK  0x0f
@@ -73,6 +77,7 @@ struct profile_sram {
 	#define mode_bits2_VA  0x00
 	#define mode_bits2_DLC 0x00
 
+*/
 
 void init_verif();
 void init_profile_sram();
