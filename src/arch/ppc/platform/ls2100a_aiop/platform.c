068fbb241566d8d7b5f70915129de2d8f9184fda
Alex Shraifel <b49560@freescale.com>
2014-05-12 14:32:22 +0300

    add support fort uart 0 in aiop

--- src/arch/ppc/platform/ls2100a_aiop/platform.c
+++ src/arch/ppc/platform/ls2100a_aiop/platform.c
@@ -306,7 +306,7 @@ static int disable_l1_cache(t_platform *pltfrm)
     return E_OK;
 }
 
-#ifdef ARENA_LEGACY_CODE
+
 /*****************************************************************************/
 static int console_print_cb(fsl_handle_t h_console_dev, uint8_t *p_data, uint32_t size)
 {
@@ -328,7 +328,7 @@ static int console_get_line_cb(fsl_handle_t h_console_dev, uint8_t *p_data, uint
 
     return (int)count;
 }
-
+#ifdef ARENA_LEGACY_CODE
 /*****************************************************************************/
 static void pltfrm_enable_local_irq_cb(fsl_handle_t h_platform)
 {
@@ -435,7 +435,7 @@ static int pltfrm_free_core_cb(fsl_handle_t h_platform)
     return E_OK;
 }
 
-#ifdef ARENA_LEGACY_CODE
+
 /*****************************************************************************/
 static int pltfrm_init_console_cb(fsl_handle_t h_platform)
 {
@@ -472,7 +472,6 @@ static int pltfrm_free_console_cb(fsl_handle_t h_platform)
 
     return E_OK;
 }
-#endif
 
 /*****************************************************************************/
 static int pltfrm_init_mem_partitions_cb(fsl_handle_t h_platform)
@@ -692,13 +691,10 @@ int platform_init(struct platform_param    *pltfrm_param,
     pltfrm_ops->f_free_timer            = NULL;
     pltfrm_ops->f_init_ipc              = NULL;
     pltfrm_ops->f_free_ipc              = NULL;
-#ifdef ARENA_LEGACY_CODE
+
     pltfrm_ops->f_init_console          = pltfrm_init_console_cb;
     pltfrm_ops->f_free_console          = pltfrm_free_console_cb;
-#else
-    pltfrm_ops->f_init_console          = NULL;
-    pltfrm_ops->f_free_console          = NULL;
-#endif
+
     pltfrm_ops->f_init_mem_partitions   = pltfrm_init_mem_partitions_cb;
     pltfrm_ops->f_free_mem_partitions   = pltfrm_free_mem_partitions_cb;
 #ifdef ARENA_LEGACY_CODE
@@ -781,7 +777,7 @@ uint32_t platform_get_system_bus_clk(fsl_handle_t h_platform)
     return (pltfrm->param.clock_in_freq_hz * 4);
 }
 
-#ifdef ARENA_LEGACY_CODE
+
 /*****************************************************************************/
 int platform_enable_console(fsl_handle_t h_platform)
 {
@@ -798,7 +794,8 @@ int platform_enable_console(fsl_handle_t h_platform)
     SANITY_CHECK_RETURN_ERROR((pltfrm->param.console_type == PLTFRM_CONSOLE_DUART), E_NOT_SUPPORTED);
 
     /* Fill DUART configuration parameters */
-    duart_uart_param.base_address       = platform_get_memory_mapped_module_base(pltfrm, FSL_OS_MOD_UART, pltfrm->param.console_id, E_MAPPED_MEM_TYPE_GEN_REGS);
+    /*TODO: the base address is hard coded to uart 0, should be modified*/
+    duart_uart_param.base_address       = SOC_PERIPH_OFF_DUART1;
     duart_uart_param.system_clock_mhz   = (platform_get_system_bus_clk(pltfrm) / 1000000);
     duart_uart_param.baud_rate          = 115200;
     duart_uart_param.parity             = E_DUART_PARITY_NONE;
@@ -869,4 +866,4 @@ int platform_disable_console(fsl_handle_t h_platform)
 
     return E_OK;
 }
-#endif
+
