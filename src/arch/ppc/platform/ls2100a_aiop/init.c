0dd77e83dac24bb560015e2cc356e52a404414ba
Ira Kalman <Ira.Kalman@freescale.com>
2014-05-13 15:47:42 +0300

    dpbp = 0

--- src/arch/ppc/platform/ls2100a_aiop/init.c
+++ src/arch/ppc/platform/ls2100a_aiop/init.c
@@ -13,7 +13,7 @@
 #include "../drivers/dplib/arch/accel/fdma.h"  /* TODO: need to place fdma_release_buffer() in separate .h file */
 #include "dplib/fsl_dpbp.h"
 
-__SHRAM uint8_t abcr_lock = 0; 
+__SHRAM uint8_t abcr_lock = 0;
 
 extern int cmdif_srv_init(void);    extern void cmdif_srv_free(void);
 extern int dpni_drv_init(void);     extern void dpni_drv_free(void);
@@ -141,7 +141,7 @@ void core_ready_for_tasks(void)
 		(~((uint32_t)sys_get_cores_mask()));
 	while(ioread32(abcr) != abrr_val) {asm{nop}}
     }
-    
+
     /* Write AIOP boot status (ABCR) */
     lock_spinlock(&abcr_lock);
     abcr_val = ioread32(abcr);
@@ -153,11 +153,11 @@ void core_ready_for_tasks(void)
 	void* abrr = UINT_TO_PTR(tmp_reg + 0x90);
 	while(ioread32(abcr) != ioread32(abrr)) {asm{nop}}
     }
-    
+
 #if (STACK_OVERFLOW_DETECTION == 1)
     booke_set_spr_DAC2(0x800);
 #endif
-    
+
     /* CTSEN = 1, finished boot, Core Task Scheduler Enable */
     booke_set_CTSCSR0(booke_get_CTSCSR0() | CTSCSR_ENABLE);
     __e_hwacceli(YIELD_ACCEL_ID); /* Yield */
@@ -258,7 +258,7 @@ int run_apps(void)
 	/* TODO: Currently creating a stub DPBP with ID=1.
 	 * Open and init calls will be replaced by 'create' when available at MC.
 	 * At that point, the DPBP ID will be provided by MC. */
-	dpbp_id = 1;
+	dpbp_id = 0;
 
 	dpbp.cidesc.regs = portal_vaddr;
 
