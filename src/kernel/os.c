952f9c07f4e84240f8f048bae4e072e30fccad7a
Danny Shultz <danny.shultz@freescale.com>
2014-05-15 23:04:12 +0300

    fixed unused variable warnings

--- src/kernel/os.c
+++ src/kernel/os.c
@@ -120,21 +120,26 @@ void spin_lock_free(spinlock_t lock)
 
 void spin_lock(spinlock_t lock)
 {
+	UNUSED(lock);
     //sys_lock_spinlock((struct spinlock *)lock);
 }
 
 void spin_unlock(spinlock_t lock)
 {
+	UNUSED(lock);
     //sys_unlock_spinlock((struct spinlock *)lock);
 }
 
 uint32_t spin_lock_irqsave(spinlock_t lock)
 {
+	UNUSED(lock);
     return 0;//sys_lock_intr_spinlock((struct spinlock *)lock);
 }
 
 void spin_unlock_irqrestore(spinlock_t lock, uint32_t irq_flags)
 {
+	UNUSED(lock);
+	UNUSED(irq_flags);
 	//sys_unlock_intr_spinlock((struct spinlock *)lock, irq_flags);
 }
 
