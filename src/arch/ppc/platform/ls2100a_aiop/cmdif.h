aa652e7967c7278b242155cfe16fa86b072cc3dc
Ira Kalman <Ira.Kalman@freescale.com>
2014-06-24 22:26:13 +0300

    Added arch file for CMDIF GPP

--- src/arch/ppc/platform/ls2100a_aiop/cmdif.h
+++ src/arch/ppc/platform/ls2100a_aiop/cmdif.h
@@ -7,10 +7,10 @@
 #error "AIOP core must be big endian\n!"
 #endif
 
-#define CPU_TO_SRV16 CPU_TO_LE16
-#define CPU_TO_SRV32 CPU_TO_LE32
+#define CPU_TO_SRV16(val) CPU_TO_LE16(val)
+#define CPU_TO_SRV32(val) CPU_TO_LE32(val)
 
-#define CPU_TO_CL16 CPU_TO_LE16
-#define CPU_TO_CL32 CPU_TO_LE32
+#define CPU_TO_CL16(val) CPU_TO_LE16(val)
+#define CPU_TO_CL32(val) CPU_TO_LE32(val)
 
 #endif /* __CMDIF_H */
