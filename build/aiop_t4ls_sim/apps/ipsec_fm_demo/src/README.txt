cc96f3cf1c10cb8e27ace199a1c2a61ddfd01122
Yariv Sheffer <yariv.sheffer@freescale.com>
2014-07-06 08:47:53 +0300

    Updated ipsec_fm_demo from ldpaa-aiop-sl-v0.4.1.update_02_ipsec

--- build/aiop_t4ls_sim/apps/ipsec_fm_demo/src/README.txt
+++ build/aiop_t4ls_sim/apps/ipsec_fm_demo/src/README.txt
@@ -1,21 +1,38 @@
-The following file includes the instructions for app_process_packet demo.
+The following file includes the instructions for the IPsec Functional Module demo (ipsec_fm_demo).
+
+===========================================
+Functional Description
+===========================================
+This demo is doing the following operations during initialization:
+- Creates an IPsec instance
+- Creates an Outbound SA descriptor
+- Creates an Inbound SA descriptor
+
+This demo is doing the following operations when a packet is injected:
+- Saves the original frame header
+- Sending the original frame to encryption, creating a new encrypted frame
+- Sending the encrypted frame to decryption, creating a new decrypted frame
+- Comparing the original frame to the decrypted frame
+- Sending the frame
+
+* See the section "TIO Output Reference" at the end of this file.
 
 ===========================================
 Setup
 ===========================================
 1. Install CW_APP_v10.0.8
-2. Download linux version of LS_SIM_RELEASE_0_8_0_0112
+2. Download linux version of LS_SIM_m0116
 3. Copy into simulator folder ls2085a_sim_init_params.cfg , ls2100_sys_test.cfg
    from aiopsl\build\aiop_t4ls_sim\sim_files.
 4. Update the “LD_LIBRARY_PATH” variable to point to simulator folder.
-   setenv LD_LIBRARY_PATH {$LD_LIBRARY_PATH}:/home/user/DPAA_SIM_RELEASE_0_8_0_0112/dtsim_release/linux64
+   setenv LD_LIBRARY_PATH {$LD_LIBRARY_PATH}:/home/user/LS_SIM_m0116/dtsim_release/linux64
 5. Copy the dpl.dtb file from aiopsl\misc\setup to simulator folder.
 6. Copy “eth_ipv4_udp.pcap” from aiopsl\misc\setup into to simulator folder
 
 ===========================================
 Execution flow
 ===========================================
-1. Build mc_app from mc_release_0.4.1 using CW_APP_v10.0.8.
+1. Build mc_app using CW_APP_v10.0.8.
 2. Build app_process_packet from aiopsl using CW_APP_v10.0.8.
 3. Copy resulting ELF file from the build project folder(aiop_app.elf) to simulator folder (same location as cfg files).
 4. Run simulator:
@@ -50,8 +67,8 @@ Possible modifications:
    mode as it was supported in previous releases, it is required to recompile
    aiopsl and app_process_packet project with AIOP_STANDALONE defined.
    Please note that the standalone mode is being phased out and has is no longer verified.
-6. The user may run without elf loader. In order to do that go to simulator folder and 
-   comment the lines marked with "#for elf loader" in cfg files: 
+6. The user may run without elf loader. In order to do that go to simulator folder and
+   comment the lines marked with "#for elf loader" in cfg files:
    "ls2100_sys_test" and "ls2085a_sim_init_params"
 
 =================
@@ -65,13 +82,72 @@ ARENA sets the following default values for every NI:
 ===========================================
   profile_id = 0 - only parser profile id 0 is supported
   QDID = 0 - Queueing destination for the enqueue
-  spid = 0 - Storage profile ID
   MTU = maximal value
 
 ==========
 ARENA APIs
 ==========
-Don't use virtual pools API, use SLAB API as demonstrated inside this demo
-For spinlock, use only: lock_spinlock and unlock_spinlock
-All other spinlock functions will be removed in future release.
+Don't use virtual pools API, use SLAB API as demonstrated inside this demo.
 
+
+=========================
+TIO Output Reference
+=========================
+This is an example of an output printed on the TIO console:
+
+
+++++
+  IPsec Demo: Doing IPsec Initialization
++++
+> TRACE [CPU00, cmdif_srv.c:429 mc_portal_isr]: got cmd @portal1
+ipsec_create_instance() completed successfully
+slab_create() completed successfully
+slab_acquire() completed successfully
+slab_acquire() completed successfully
+ipsec_add_sa_descriptor(encryption) succeeded
+Encryption handle = 0x0_5834efa0
+ipsec_add_sa_descriptor(decryption) succeeded
+Decryption handle = 0x0_5834ed60
+IPsec Demo: IPsec Initialization completed
+> INFO [CPU 0, init.c:167 core_ready_for_tasks]: AIOP core 0 completed boot sequence
+> INFO [CPU 0, init.c:172 core_ready_for_tasks]: AIOP boot finished; ready for tasks...
+IPsec Demo: Core 0 Received Frame
+Encryption handle = 0x0_5834efa0
+Decryption handle = 0x0_5834ed60
+IPSEC: frame header before encryption
+00  00 00 01 00 00 01 00 10  94 00 00 02 08 00 45 00
+10  00 6e 00 00 00 00 ff 11  3a 26 c0 55 01 02 c0 00
+20  00 01 04 00 04 00 00 5a  ff ff 00 00 00 00 00 00
+30  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
+40  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
+50  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
+60  00 00 00 00 00 00 00 00  f5 d0 64 51 ac 9f 69 d4
+70  d3 f7 39 6e 20 0e 97 b7  e9 e4 56 3a
+
+IPSEC: Starting Encryption
+ipsec_frame_encrypt() completed successfully
+IPSEC: frame header after encryption
+00  00 00 01 00 00 01 00 10  94 00 00 02 08 00 45 db
+10  00 98 12 34 00 00 ff 32  37 eb 45 a4 e1 4c ed 03
+20  5c 45 00 00 00 00 00 00  00 00 45 00 00 6e 00 00
+30  00 00 ff 11 3a 26 c0 55  01 02 c0 00 00 01 04 00
+40  04 00 00 5a ff ff 00 00  00 00 00 00 00 00 00 00
+50  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
+60  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
+70  00 00 00 00 00 00 00 00  00 00 00 00
+
+IPSEC: Starting Decryption
+ipsec_frame_decrypt() completed successfully
+IPSEC: frame header after decryption
+00  00 00 01 00 00 01 00 10  94 00 00 02 08 00 45 00
+10  00 6e 00 00 00 00 ff 11  3a 26 c0 55 01 02 c0 00
+20  00 01 04 00 04 00 00 5a  ff ff 00 00 00 00 00 00
+30  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
+40  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
+50  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00
+60  00 00 00 00 00 00 00 00  f5 d0 64 51 ac 9f 69 d4
+70  d3 f7 39 6e 20 0e 97 b7  e9 e4 56 3a
+
+SUCCESS: frame after decryption the same as origin
+IPsec Demo: Core 0 Sending Frame
+IPsec Demo: Done Sending Frame
--- build/aiop_t4ls_sim/apps/ipsec_fm_demo/src/ipsec_fm_demo.c
+++ build/aiop_t4ls_sim/apps/ipsec_fm_demo/src/ipsec_fm_demo.c
@@ -7,12 +7,11 @@
 #include "fsl_parser.h"
 #include "general.h"
 #include "fsl_dbg.h"
-#include "fsl_cmdif_server.h"
+#include "kernel/fsl_cmdif_server.h"
 #include "dplib/fsl_cdma.h"
 #include "dplib/fsl_ipsec.h"
-//#include "lib/fsl_slab.h"
-#include "slab.h"
-#include "ipsec.h"
+#include "ls2085_aiop/fsl_platform.h"
+#include "lib/fsl_slab.h"
 
 int app_init(void);
 void app_free(void);
@@ -28,18 +27,11 @@ void ipsec_print_frame(void);
 __SHRAM ipsec_instance_handle_t ipsec_instance_handle;
 __SHRAM ipsec_instance_handle_t ipsec_sa_desc_outbound;
 __SHRAM ipsec_instance_handle_t ipsec_sa_desc_inbound; 
+__SHRAM uint32_t frame_number; 
 
-#ifdef DO_IPSEC_WA
-	extern __SHRAM struct ipsec_global_params global_params;
-#endif
-	
 __HOT_CODE static void app_process_packet_flow0 (dpni_drv_app_arg_t arg)
 {
 	int      err = 0;
-	const uint16_t ipv4hdr_length = sizeof(struct ipv4hdr);
-	uint16_t ipv4hdr_offset = 0;
-	uint8_t *p_ipv4hdr = 0;
-	uint32_t src_addr = 0x10203040;// new ipv4 src_addr
 	uint32_t enc_status = 0;
 	uint32_t dec_status = 0;
 	int i = 0;
@@ -49,40 +41,30 @@ __HOT_CODE static void app_process_packet_flow0 (dpni_drv_app_arg_t arg)
 
 	eth_pointer_byte = (uint8_t *)PARSER_GET_ETH_POINTER_DEFAULT();
 	uint32_t frame_len = LDPAA_FD_GET_LENGTH(HWC_FD_ADDRESS);
+	uint16_t seg_len = PRC_GET_SEGMENT_LENGTH();
 	
 	ipsec_handle_t ws_desc_handle_outbound = ipsec_sa_desc_outbound;
 	ipsec_handle_t ws_desc_handle_inbound = ipsec_sa_desc_inbound; 
 	
-	uint32_t shared_desc[30];
-
-	fsl_os_print("IPsec Demo: Core %d Received Frame\n", core_get_id());
-
-	handle_high = (uint32_t)((ws_desc_handle_outbound & 0xffffffff00000000)>>32);
-	handle_low = (uint32_t)(ws_desc_handle_outbound & 0x00000000ffffffff);
-	fsl_os_print("Encryption handle addr = 0x%x_%x\n", handle_high, handle_low);
+	frame_number ++;
 	
-	handle_high = (uint32_t)((ws_desc_handle_inbound & 0xffffffff00000000)>>32);
-	handle_low = (uint32_t)(ws_desc_handle_inbound & 0x00000000ffffffff);
-	fsl_os_print("Decryption handle addr = 0x%x_%x\n", handle_high, handle_low);
-	
-	/* Debug */
-	cdma_read(
-			shared_desc, /* void *ws_dst */
-			(ws_desc_handle_outbound + 128 + 64), /* uint64_t ext_address */
-			sizeof(shared_desc) /* uint16_t size */
-			);
-	fsl_os_print("IPSEC: Outbound Shared Descriptor, before encryption\n");
-	for(i = 0; i < 30 ;i ++)
-	{
-		fsl_os_print("Desc word %d = 0x%x\n", i, shared_desc[i]);
-	}
+	fsl_os_print("IPsec Demo: Core %d Received Frame number %d\n", 
+			core_get_id(), frame_number);
 
-	fsl_os_print("\n");
+	handle_high = 
+			(uint32_t)((ws_desc_handle_outbound & 0xffffffff00000000)>>32);
+	handle_low = 
+			(uint32_t)(ws_desc_handle_outbound & 0x00000000ffffffff);
+	fsl_os_print("Encryption handle = 0x%x_%x\n", handle_high, handle_low);
 	
-	/** Debug End **/
+	handle_high = 
+			(uint32_t)((ws_desc_handle_inbound & 0xffffffff00000000)>>32);
+	handle_low = 
+			(uint32_t)(ws_desc_handle_inbound & 0x00000000ffffffff);
+	fsl_os_print("Decryption handle = 0x%x_%x\n", handle_high, handle_low);
 	
 	/* preserve original frame */
-	for(i = 0; i < (frame_len) ;i ++)
+	for(i = 0; ((i<frame_len) && (i<seg_len));i ++)
 	{
 		frame_before_encr[i] =  *eth_pointer_byte;
 		eth_pointer_byte++;
@@ -106,22 +88,10 @@ __HOT_CODE static void app_process_packet_flow0 (dpni_drv_app_arg_t arg)
 	else
 		fsl_os_print("ipsec_frame_encrypt() completed successfully\n");
 	
-	/* Debug */
-	cdma_read(
-			shared_desc, /* void *ws_dst */
-			(ws_desc_handle_outbound + 128 + 64), /* uint64_t ext_address */
-			sizeof(shared_desc) /* uint16_t size */
-			);
-	fsl_os_print("IPSEC: Outbound Shared Descriptor, AFTER encryption\n");
-	for(i = 0; i < 30 ;i ++)
-	{
-		fsl_os_print("Desc word %d = 0x%x\n", i, shared_desc[i]);
-	}
+	if (enc_status)
+		fsl_os_print("ERROR: SEC Encryption Failed (enc_status = 0x%x)\n", 
+				enc_status);
 
-	fsl_os_print("\n");
-	
-	/** Debug End **/
-	
 	/* Run parser on new (encrypted) frame */ 
 	parse_result_generate_default (PARSER_NO_FLAGS);
 	
@@ -129,7 +99,6 @@ __HOT_CODE static void app_process_packet_flow0 (dpni_drv_app_arg_t arg)
 	/* Print header */
 	ipsec_print_frame();
 	fsl_os_print("\n");
-	
 	
 	fsl_os_print("IPSEC: Starting Decryption\n");
 	err = ipsec_frame_decrypt(
@@ -143,6 +112,10 @@ __HOT_CODE static void app_process_packet_flow0 (dpni_drv_app_arg_t arg)
 	else
 		fsl_os_print("ipsec_frame_decrypt() completed successfully\n");
 	
+	if (dec_status)
+		fsl_os_print("ERROR: SEC Decryption Failed (dec_status = 0x%x)\n", 
+				dec_status);
+
 	fsl_os_print("IPSEC: frame header after decryption\n");
 	/* Print header */
 	ipsec_print_frame();
@@ -152,22 +125,30 @@ __HOT_CODE static void app_process_packet_flow0 (dpni_drv_app_arg_t arg)
 	err = 0;
 	eth_pointer_byte = (uint8_t *)PARSER_GET_ETH_POINTER_DEFAULT();
 	frame_len = LDPAA_FD_GET_LENGTH(HWC_FD_ADDRESS);
-
-	for(i = 0; i < (frame_len) ;i ++)
-	{
-		if (frame_before_encr[i] !=  *eth_pointer_byte) {
-			fsl_os_print("ERROR: frame after decryption differ from origin\n");
-			err = 1;
-			break;
+	
+	if (frame_len < 20) {
+		fsl_os_print("ERROR: incorrect frame length (FD[length] = %d)\n", 
+				frame_len);
+		err = 1;
+	} else {
+		for(i = 0; ((i<frame_len) && (i<seg_len));i ++)
+		{
+			if (frame_before_encr[i] !=  *eth_pointer_byte) {
+				fsl_os_print(
+						"ERROR: frame after decryption differ from origin\n");
+				err = 1;
+				break;
+			}
+			eth_pointer_byte++;
 		}
-		eth_pointer_byte++;
 	}
 	
 	if (!err)
 		fsl_os_print("SUCCESS: frame after decryption the same as origin\n");
 	
 	
-	fsl_os_print("IPsec Demo: Core %d Sending Frame\n", core_get_id());
+	fsl_os_print("IPsec Demo: Core %d Sending Frame number %d\n", 
+			core_get_id(), frame_number);
 
 	dpni_drv_send(APP_NI_GET(arg));
 	
@@ -252,9 +233,9 @@ int app_init(void)
 		/* Every ni will have 1 flow */
 		uint32_t flow_id = 0;
 		err = dpni_drv_register_rx_cb((uint16_t)ni/*ni_id*/,
-		                              (uint16_t)flow_id/*flow_id*/,
-		                              app_process_packet_flow0, /* callback for flow_id*/
-		                              (ni | (flow_id << 16)) /*arg, nic number*/);
+		         (uint16_t)flow_id/*flow_id*/,
+		         app_process_packet_flow0, /* callback for flow_id*/
+		         (ni | (flow_id << 16)) /*arg, nic number*/);
 		if (err) return err;
 	}
 
@@ -276,10 +257,6 @@ int ipsec_app_init(void)
 	int err  = 0;
 	struct ipsec_descriptor_params params;
 	uint32_t outer_ip_header[5];
-#ifdef DO_IPSEC_WA
-	int      num_filled_buffs;
-	uint16_t bpid; // TODO: TMP workaround
-#endif
 	struct slab *slab_handle = NULL;
 	uint32_t handle_high, handle_low;
 
@@ -293,6 +270,8 @@ int ipsec_app_init(void)
 	ipsec_handle_t ws_desc_handle_outbound = 0;
 	ipsec_handle_t ws_desc_handle_inbound = 0; 
 	
+	frame_number = 0;
+	
 	fsl_os_print("\n++++\n  IPsec Demo: Doing IPsec Initialization\n+++\n");
 	err = ipsec_create_instance(
 			10, /* committed sa num */
@@ -302,33 +281,15 @@ int ipsec_app_init(void)
 			&ws_instance_handle);
 	if (err) {
 		fsl_os_print("ERROR: ipsec_create_instance() failed\n");
-		fsl_os_print("ipsec_create_instance return status = %d (0x%x)\n", err, err);
+		fsl_os_print("ipsec_create_instance return status = %d (0x%x)\n", 
+				err, err);
 	} else {
 		fsl_os_print("ipsec_create_instance() completed successfully\n");
 	}
 	
 	ipsec_instance_handle = ws_instance_handle;
 	
-	/*******************  TMP Workaround ***************************/
-#ifdef DO_IPSEC_WA
-	err = slab_find_and_fill_bpid(
-			10, /* uint32_t num_buffs, */
-			512, /*	uint16_t buff_size, */
-			8, /*	uint16_t alignment, */
-			MEM_PART_DP_DDR, /*	uint8_t  mem_pid, */
-			&num_filled_buffs, /* int      *num_filled_buffs, */
-			&bpid /* uint16_t *bpid */
-			);
-
-	if (err)
-		fsl_os_print("ERROR: slab_find_and_fill_bpid() failed\n");	
-	else
-		fsl_os_print("slab_find_and_fill_bpid() returned BPID = %d\n", bpid);	
-	
-	global_params.desc_bpid = bpid;
-#endif
-	/*******************  End of TMP Workaround ***************************/
-
+	/* Allocate buffers for the Keys */
 	err = slab_create(
 			10, /* uint32_t    num_buffs */
 			10, /* uint32_t    max_buffs */
@@ -381,7 +342,6 @@ int ipsec_app_init(void)
 			auth_key_addr, /* ext_address */
 			&auth_key, /* ws_src */
 			16); /* uint16_t size */
-	
 	
 	/* Encryption Descriptor Parameters */
 	
@@ -436,67 +396,21 @@ int ipsec_app_init(void)
 			ws_instance_handle,
 			&ws_desc_handle_outbound);
 
-	handle_high = (uint32_t)((ws_desc_handle_outbound & 0xffffffff00000000)>>32);
-	handle_low = (uint32_t)(ws_desc_handle_outbound & 0x00000000ffffffff);
+	handle_high = 
+			(uint32_t)((ws_desc_handle_outbound & 0xffffffff00000000)>>32);
+	handle_low = 
+			(uint32_t)(ws_desc_handle_outbound & 0x00000000ffffffff);
 	
 	if (err) {
 		fsl_os_print("ERROR: ipsec_add_sa_descriptor(encryption) failed\n");
-		fsl_os_print("ipsec_add_sa_descriptor return status = %d (0x%x)\n", err, err);
+		fsl_os_print("ipsec_add_sa_descriptor return status = %d (0x%x)\n", 
+				err, err);
 	} else {
 		fsl_os_print("ipsec_add_sa_descriptor(encryption) succeeded\n");
-		fsl_os_print("Encryption handle addr = 0x%x_%x\n", handle_high, handle_low);
+		fsl_os_print("Encryption handle = 0x%x_%x\n", handle_high, handle_low);
 	}
 
 	ipsec_sa_desc_outbound = ws_desc_handle_outbound;
-
-#ifdef IPSEC_SWAP_SD	
-	/* Debug - swap descriptor */
-	uint32_t tmp_sd[30];
-	uint32_t tmp_word[4];
-	uint64_t sd_addr = ws_desc_handle_outbound + 192;
-	cdma_read(
-			tmp_sd, /* void *ws_dst */
-			sd_addr, /* uint64_t ext_address */
-			sizeof(tmp_sd) /* uint16_t size */
-			);
-	
-	fsl_os_print("IPSEC: Outbound Shared Descriptor, before swap\n");
-	for(int i = 0; i < 30 ;i ++)
-	{
-		fsl_os_print("Desc word %d = 0x%x\n", i, tmp_sd[i]);
-	}
-
-	for (int i = 0; i < 30 ;i ++)
-	{
-		tmp_word[3] = ((tmp_sd[i] & 0x000000FF)<<24);
-		tmp_word[2] = ((tmp_sd[i] & 0x0000FF00)>>8)<<16;
-		tmp_word[1] = ((tmp_sd[i] & 0x00FF0000)>>16)<<8;
-		tmp_word[0] = ((tmp_sd[i] & 0xFF000000)>>24);
-		
-		tmp_sd[i] =  tmp_word[3] | tmp_word[2] | tmp_word[1] | tmp_word[0];
-	}
-
-	cdma_write(
-			sd_addr, /* ext_address */
-			tmp_sd, /* ws_src */
-			(sizeof(tmp_sd))); /* size */
-	
-	cdma_read(
-			tmp_sd, /* void *ws_dst */
-			sd_addr, /* uint64_t ext_address */
-			sizeof(tmp_sd) /* uint16_t size */
-			);
-	fsl_os_print("IPSEC: Outbound Shared Descriptor, after swap\n");
-	for(int i = 0; i < 30 ;i ++)
-	{
-		fsl_os_print("Desc word %d = 0x%x\n", i, tmp_sd[i]);
-	}
-
-	fsl_os_print("\n");
-	
-	/* End Debug - swap descriptor */
-#endif /* IPSEC_SWAP_SD */	
-		
 	
 	/* Inbound (decryption) parameters */
 	params.direction = IPSEC_DIRECTION_INBOUND; /**< Descriptor direction */
@@ -540,12 +454,12 @@ int ipsec_app_init(void)
 	
 	if (err) {
 		fsl_os_print("ERROR: ipsec_add_sa_descriptor(decryption) failed\n");
-		fsl_os_print("ipsec_add_sa_descriptor return status = %d (0x%x)\n", err, err);
+		fsl_os_print("ipsec_add_sa_descriptor return status = %d (0x%x)\n",
+				err, err);
 	} else {
 		fsl_os_print("ipsec_add_sa_descriptor(decryption) succeeded\n");
-		fsl_os_print("Decryption handle addr = 0x%x_%x\n", handle_high, handle_low);
+		fsl_os_print("Decryption handle = 0x%x_%x\n", handle_high, handle_low);
 	}
-
 	
 	ipsec_sa_desc_inbound = ws_desc_handle_inbound;
 	
@@ -588,5 +502,4 @@ void ipsec_print_frame(void) {
 	
 	if ((i%16) != 0)
 		fsl_os_print("\n");
-}
-
+} /* End of ipsec_print_frame */
