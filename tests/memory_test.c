d1a4e7f855637b70f1478c10c279e8b2c52d8388
Boris Alesker <BorisA@freescale.com>
2014-07-14 15:29:13 +0300

    enable cdma_peb_test

--- tests/memory_test.c
+++ tests/memory_test.c
@@ -7,7 +7,8 @@ int cdma_peb_test();
 int cdma_dpddr_test();
 int cdma_systemddr_test();
 
-#define PEB_AIOP_OFFSET 0x80000000
+/*PEB_AIOP_OFFSET should be compatible with #define MEMORY_INFO that is defined in init.c */
+#define PEB_AIOP_OFFSET 0x80200000 
 #define DP_DDR_AIOP_OFFSET 0x58000000
 
 static const uint32_t value = 0xdeadbeef;
@@ -15,14 +16,11 @@ static const uint32_t value = 0xdeadbeef;
 int memory_test()
 {
 	int err1 = 0,err2 = 0,err3 = 0;
-	/*
-	 * Temporary until finding out why simulator throws an assert in CDMA with physical address.
 	err1 = cdma_peb_test();
 	if(!err1)
 		fsl_os_print("cdma_peb_test succeeded\n"); 
 	else
 		fsl_os_print("cdma_peb_test failed\n");	
-		*/
 	err2 = cdma_dpddr_test();
 	if(!err2)
 		fsl_os_print("cdma_dpddr_test succeeded\n"); 
@@ -43,17 +41,13 @@ int cdma_peb_test()
 {
 	uint32_t *addr = UINT_TO_PTR(PEB_AIOP_OFFSET);
 	uint32_t *addr_offs_4 = UINT_TO_PTR(PEB_AIOP_OFFSET + 4);
-	//uint32_t variable_on_stack = 0; // variable on stack ( workspace) for cdma read/write.
-	uint32_t variable_on_stack = 0xdeadbeef; // variable on stack ( workspace) for cdma read/write.	
+	uint32_t variable_on_stack = 0; // variable on stack ( workspace) for cdma read/write.	
 	dma_addr_t phys_addr = 0;
 	phys_addr = fsl_os_virt_to_phys(addr);
-	/*
 	iowrite32(value, addr);
-	phys_addr = fsl_os_virt_to_phys(addr);
 	cdma_read(&variable_on_stack,phys_addr, sizeof(value));
 	if(ioread32(&variable_on_stack) != value)
 		return EIO;
-	*/
 	cdma_write(phys_addr + 4, &variable_on_stack, sizeof(value));
 	if(ioread32(addr_offs_4) != value)
 		return EIO;	
