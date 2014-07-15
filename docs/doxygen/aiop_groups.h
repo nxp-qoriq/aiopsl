d46056ed97d6cfb83d85d0a1331f12a1bc62515a
Danny Shultz <danny.shultz@freescale.com>
2014-07-15 19:31:07 +0300

    updated groups file

--- docs/doxygen/aiop_groups.h
+++ docs/doxygen/aiop_groups.h
@@ -1,11 +1,58 @@
-/**************************************************************************//**
- @Group		fsl_lib_g   Utility Library 
+/**
+ @Group	fsl_lib_g   Utility Library 
  @{
- *//***************************************************************************/
+ */ 
+	/** 
+	@Group FSL_AIOP_SPINLOCK  Spin-locks
+	@{ @}
+	*/
+/** 
+ @}
+ */ /* end of fsl_lib_g group */
+
+/**
+@Group	grp_dplib_aiop	DPLIB
+@{
+*/
+	/**
+	@Group	dpni_g	DPNI (AIOP Data Path Network Interface API)
+	@{ @}
+	*/
+
+/** 
+ @}
+ */ /* end of grp_dplib_aiop group */
+
  
-	/** @Group FSL_AIOP_SPINLOCK  Spin-locks
-	@{ @}*/
+ /**
+ @Group		group_util   Utilities
+ @{
+ */
+ 	/**
+	@Group	net_utils_g AIOP Service Layer Network Utilities 
+    @{ @}
+	*/
  
- /********************** end of fsl_lib_g group ****************************//**
- @} 
- *//***************************************************************************/
+	/**
+	@Group	FSL_DEBUG_GROUP Debug Utilities
+    @{ @}
+	*/
+	
+	/**
+	@Group	accessor_g Accessor API
+    @{ @}
+	*/
+	
+	/**
+	@Group	error_g Error Handling  
+    @{ @}
+	*/
+	
+	/**
+	@Group	time_g Time Queries 
+    @{ @}
+	*/
+ 
+ /** 
+ @}
+ */ /* end of group_util group */
