03ce7ee5fadd16f8a2f93d42593bd9c85c8f96f3
Danny Shultz <danny.shultz@freescale.com>
2014-07-17 08:48:16 +0300

    updated doxygen fetch script

--- docs/doxygen/fetch_headers.bat
+++ docs/doxygen/fetch_headers.bat
@@ -1,10 +1,18 @@
 @echo off
 
-cd ..\..
+echo Copyright (c) 2014 Freescale Semiconductor, Inc. All rights reserved.
+echo Fetching header files for doxygen:
 
+cd ..\..
 for /f "tokens=*" %%a in (docs\doxygen\DoxygenFileList.txt) do (
 	(echo %%a | find /I ".h" >NULL) && (
 		echo %%a
-		For /F %%B in ('dir /S /B %%a') do copy /Y %%B docs\doxygen\%%a
+		del /F /Q docs\doxygen\%%a 2> null
+		
+		For /F %%B in ('dir /S /B %%a') do (
+			copy /Y %%B docs\doxygen\%%a > null
+		)
 	)
 )
+
+pause
