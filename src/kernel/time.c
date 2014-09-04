4ac6e02584d4873be6948fae16571bab998cdc57
Alex Shraifel <b49560@freescale.com>
2014-09-04 18:06:46 +0300

    Updating implementation for time queries. Optimization was made for fsl_get_time_ms.

--- src/kernel/time.c
+++ src/kernel/time.c
@@ -29,9 +29,13 @@
 #include "fsl_io_ccsr.h"
 #include "fsl_sys.h"
 
+
+
+__SHRAM uint64_t time_epoch_to_midnight = 0; /*microseconds since epoch till midnight*/
+
 __HOT_CODE int _gettime(uint64_t *time)
 {
-	uint32_t TSCRU1, TSCRU2, TSCRL;
+	uint32_t TEMP, TSCRU, TSCRL;
 	uint64_t temp_val;
 	struct aiop_tile_regs *aiop_regs = (struct aiop_tile_regs *)
 		       		       sys_get_handle(FSL_OS_MOD_AIOP_TILE, 1);
@@ -42,15 +46,68 @@ __HOT_CODE int _gettime(uint64_t *time)
 		return -ENAVAIL;
 #endif
 
-	TSCRU1 = ioread32_ccsr(&aiop_regs->cmgw_regs.tscru);
+	TEMP = ioread32_ccsr(&aiop_regs->cmgw_regs.tscru);
 	TSCRL = ioread32_ccsr(&aiop_regs->cmgw_regs.tscrl);
-	if ((TSCRU2=ioread32_ccsr(&aiop_regs->cmgw_regs.tscru)) > TSCRU1 )
+	if ((TSCRU = ioread32_ccsr(&aiop_regs->cmgw_regs.tscru)) > TEMP )
 		TSCRL = 0;
-	else if(TSCRU2 < TSCRU1) /*something wrong while reading*/
+	else if(TSCRU < TEMP) /*something wrong while reading*/
 		return -EACCES;
 
-	temp_val = (uint64_t)(TSCRU2) << 32;
-	temp_val |= (TSCRL); /*temp_val holds time in nanoseconds*/
-	*time = (temp_val / 1000); /*convert to microseconds*/
-	 return 0;
+	TEMP = udiv1000(TSCRU);
+	TSCRU = TSCRU-(1000*TEMP); // remainder
+	temp_val = TEMP;
+	temp_val <<= 32;
+	TSCRU = (TSCRU << 16) | (TSCRL >> 16);
+	TEMP = udiv1000(TSCRU);
+	TSCRU = TSCRU - (1000 * TEMP); // remainder
+	temp_val |= ((uint64_t) TEMP) << 16;
+	TSCRU = (TSCRU << 16) | (TSCRL & 0xffff);
+	temp_val |= udiv1000(TSCRU);
+
+	*time = temp_val;
+	return 0;
 }
+
+/*****************************************************************************/
+__HOT_CODE int fsl_get_time_ms(uint32_t *time)
+{
+	uint64_t time_us;
+	uint64_t local_epoch_to_midnight;
+	int err;
+
+	err = _gettime(&time_us);
+	if(err < 0)
+		return err;
+
+	local_epoch_to_midnight = LLLDW(0, &time_epoch_to_midnight);
+	time_us -= local_epoch_to_midnight;
+	if(time_us < 86400000000) { /*86400000000 = 24 Hours in microseconds (24*60*60*1000*1000) */
+		*time =(uint32_t) (ulldiv1000(time_us));
+		return 0;
+	}
+	else {
+		time_us += local_epoch_to_midnight;
+		local_epoch_to_midnight = time_us - (time_us % 86400000000);
+		*time = (uint32_t) (ulldiv1000(time_us - local_epoch_to_midnight));
+		LLSTDW(local_epoch_to_midnight,0 , &time_epoch_to_midnight);
+		return 0;
+	}
+}
+
+/*****************************************************************************/
+int fsl_get_time_since_epoch_ms(uint64_t *time)
+{
+	int err = _gettime(time);
+	if(err < 0)
+		return err;
+	*time = ulldiv1000(*time);
+	return 0;
+}
+
+/*****************************************************************************/
+__HOT_CODE uint32_t fsl_os_current_time(void)
+{
+	return 0;
+}
+
+
