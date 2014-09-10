47047e44818114b95a02bf61a997e8b61f7ac54c
Doron Ben Zion <Doron.B@freescale.com>
2014-09-10 06:40:11 +0300

    ENGR00330924 fix

--- src/drivers/dplib/arch/accel/table.c
+++ src/drivers/dplib/arch/accel/table.c
@@ -690,6 +690,10 @@ int table_lookup_by_keyid_default_frame(enum table_hw_accel_id acc_id,
 	case (TABLE_HW_STATUS_EOFH):
 		status = -EIO;
 		break;
+	/*TODO EOFH with LOOKUP hit/miss */
+	case (TABLE_HW_STATUS_EOFH | TABLE_HW_STATUS_MISS):
+		status = -EIO;
+		break;
 	default:
 		/* Call fatal error handler */
 		table_exception_handler_wrp(
