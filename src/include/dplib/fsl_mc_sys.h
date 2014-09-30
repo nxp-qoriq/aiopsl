a810293969bd7397094cb77c93983b651ce45aa3
Daniela Ferber <daniela.ferber@freescale.com>
2014-09-30 16:33:23 +0300

    fixed compilation error

--- src/include/dplib/fsl_mc_sys.h
+++ src/include/dplib/fsl_mc_sys.h
@@ -2,14 +2,14 @@
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of Freescale Semiconductor nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Freescale Semiconductor nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
  *
  *
  * ALTERNATIVELY, this software may be distributed under the terms of the
@@ -68,9 +68,9 @@ static inline uint64_t iova_alloc(size_t size)
 #else /* __linux__ */
 
 #include "common/types.h"
-#include "common/errors.h"
-#include "common/fsl_malloc.h"
-#include "common/io.h"
+#include "fsl_errors.h"
+#include "fsl_io.h"
+#include "fsl_malloc.h"
 #define __iomem
 
 static inline void * iova_alloc(size_t size)
