b2e4f01f022a5a20ac46988b08389407c0961cb6
Danny Shultz <danny.shultz@freescale.com>
2015-01-06 10:36:54 +0200

    noreturn on fsl_os_exit()

--- src/kernel/os.c
+++ src/kernel/os.c
@@ -54,7 +54,7 @@ static int vsnprintf_lite(char *buf, size_t size, const char *fmt, va_list args)
 static char *number(char *str, uint64_t num, uint8_t base, uint8_t type, size_t *max_size);
 static void fsl_os_print_boot(const char *format, va_list args);
 /*****************************************************************************/
-void fsl_os_exit(int status)
+__NORETURN void fsl_os_exit(int status)
 {
     exit(status);
 }
