3ccfe96054aa36b829ba6b0f4ef81e6f1b3e95ac
Danny Shultz <danny.shultz@freescale.com>
2015-01-07 09:45:06 +0200

    update all copyrights

--- misc/tools/copyrights/update_year_copyrights.sh
+++ misc/tools/copyrights/update_year_copyrights.sh
@@ -1,5 +1,9 @@
 git grep -l Copyright | while read p
 do
     sed -i 's/Copyright 2014 Freescale/Copyright 2014-2015 Freescale/' ${p}
-	echo "Updated: ${p}"
+	echo "Updated std: ${p}"
+	
+	sed -i 's/Copyright 2013-2014 Freescale Semiconductor Inc/Copyright 2014-2015 Freescale Semiconductor Inc/' ${p}
+	echo "Updated flb: ${p}"
+	
 done
