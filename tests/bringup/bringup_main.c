89b763f095a93b7467ac1e4a63d7b8f5702e3643
Danny Shultz <danny.shultz@freescale.com>
2015-01-11 09:23:21 +0200

    updated for aiop

--- tests/bringup/bringup_main.c
+++ tests/bringup/bringup_main.c
@@ -1,256 +1,180 @@
+/*
+ * Copyright 2014-2015 Freescale Semiconductor, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Freescale Semiconductor nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Freescale Semiconductor ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Freescale Semiconductor BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "common/types.h"
 #include "common/fsl_stdio.h"
 #include "fsl_smp.h"
-#include "fsl_malloc.h"
-#include "common/fsl_string.h"
-#include "kernel/layout.h"
-#include "dtc/dpc.h"
-#include "fsl_dpmng_mc.h"
-#include "fsl_dpaiop_mc.h"
-#include "drivers/fsl_aiop.h"
-#include "platform.h"
-#include "mpu.h"
-#include "dtc/dpl.h"
+#include "fsl_dbg.h"
+#include "cmgw.h"
 
-#include "sys.h"
 #include "bringup_tests.h"
 
-extern int sys_init(void);
-extern void sys_free(void);
-extern int global_init(void);
-extern int global_free(void);
-extern int global_post_init(void);
-extern int run_apps(void);
-
-extern int sys_lo_process(void);
-extern int aiop_drv_init(void);
-
-
-static void load_aiop(void)
-{
-	struct dpaiop *dpaiop;
-	struct dpaiop_run_cfg run_cfg = {0};
-	struct dpaiop_load_cfg load_cfg = {0};
-	int err;
-
-	dpaiop = sys_get_handle(FSL_OS_MOD_DPAIOP, 1, 0);
-
-	if (dpaiop)
-	{
-		load_cfg.img_iova = (void *)0x1F800000;
-		load_cfg.options = 0x8000000000000000 |
-			DPAIOP_LOAD_OPT_DEBUG;
-		err = dpaiop_load(dpaiop, &load_cfg);
-
-		if (!err) {
-			run_cfg.cores_mask = 0xffff;
-			run_cfg.options = DPAIOP_RUN_OPT_DEBUG;
-			err = dpaiop_run(dpaiop, &run_cfg);
-#if 0
-			if (!err)
-			{
-				do {
-					dpaiop_get_status(dpaiop, &status);
-				} while (status == AIOP_TILE_STATUS_BOOT_ONGOING)
-
-				if (status == AIOP_TILE_STATUS_RUNNING)
-				{
-					pr_info("=================================================================\n");
-					pr_info("AIOP Boot Completed!\n");
-					pr_info("=================================================================\n");
-				}
-				else
-				{
-					pr_info("=================================================================\n");
-					pr_info("AIOP Run Failed!\n");
-					pr_info("=================================================================\n");
-				}
-			}
-#endif
-			if (err)
-			{
-				pr_info("=================================================================\n");
-				pr_info("AIOP Run Failed!\n");
-				pr_info("=================================================================\n");
-			}
-		}
-		else
-		{
-			pr_info("=================================================================\n");
-			pr_info("AIOP Load Failed!\n");
-			pr_info("=================================================================\n");
-		}
-	}
-	else
-		pr_info("No AIOP in layout\n");
-
-}
+//extern int sys_init(void);
+//extern void sys_free(void);
+//extern int global_early_init(void);
+//extern int global_init(void);
+//extern int tile_init(void);
+//extern int cluster_init(void);
+//extern int global_post_init(void);
+//extern int apps_early_init(void);
+//extern int run_apps(void);
+//extern void core_ready_for_tasks(void);
+//extern int dpni_drv_enable_all(void);
+//#if (STACK_OVERFLOW_DETECTION == 1)
+//__COLD_CODE static inline void configure_stack_overflow_detection(void)
+//{
+//    /* DBCR2 */
+//    booke_set_spr_DBCR2(booke_get_spr_DBCR2() | 0x00c00000);
+//
+//    /* DBCR4 */
+//    asm {
+//        mfspr   r6,DBCR4
+//        ori r6, r6, 0x0080 /* DAC1CFG */
+//        mtspr   DBCR4,r6
+//        isync
+//    }
+////  booke_set_spr_DBCR4(booke_get_spr_DBCR4() | 0x00000080);
+////  booke_instruction_sync();
+//
+//    /* DBCR0 */
+//    booke_set_spr_DBCR0(booke_get_spr_DBCR0() | 0x400f0000);
+//    booke_instruction_sync();
+//
+//    /* initiate DAC registers */
+//    booke_set_spr_DAC1(0x400);
+//    booke_set_spr_DAC2(0x8000);
+//}
+//#endif
 
 /*****************************************************************************/
 int main(int argc, char *argv[])
-
 {
-	int err = 0;
-//	int is_master_core;
-//	struct mc_version mc_version;
-//	uint32_t uboot_init_word;
+    int err = 0;
+//    int is_master_core;
+UNUSED(argc);UNUSED(argv);
 
-	/* so sys_is_master_core() will work */
-	extern t_system sys; /* Global System Object */
-	uint32_t        core_id = core_get_id();
-	sys.is_master_core[core_id]       = (int)(0x1 & (1ULL << core_id));
-	
-	UNUSED(argc); UNUSED(argv);
+    /* Initiate small data area pointers at task initialization */
+    asm {
+        mtdcr dcr469,r2 // INITR2
+        mtdcr dcr470,r13// INITR13
+    }
 	
 #if (TEST_MEM_ACCESS == ON)
+	/* memory access test */
 	err = mem_standalone_init();
-	if(err) return err;
+	err = mem_test();
+#endif /* TEST_MEM_ACCESS */
 
-//	sys_barrier();
-	
-	err =  mem_test();
-	if(err) return err;
-#endif
-
-#if (TEST_CONSOLE_PRINT == ON)
-	err = console_print_init();
-	if(err) return err;
-
-	err =  console_print_test();
-	if(err) return err;
-#endif
-
-#if (TEST_HW_SEM == ON)
-	err = hw_sem_standalone_init();
-	if(err) return err;
-	
-//	sys_barrier();
-	
-	err = hw_sem_test();
-	if(err) return err;
-#endif
-	
-#if (TEST_SPINLOCK == ON)
-	err = spinlock_standalone_init();
-	if(err) return err;
-	
-//	sys_barrier();
-	
-	err = spinlock_test();
-	if(err) return err;
-#endif
-	
-#if (TEST_MPIC == ON)
-	err = mpic_standalone_init();
-	if(err) return err;
-	
-//	sys_barrier();
-	
-	err = mpic_test();
-	if(err) return err;
-#endif
-	
-#if (TEST_MPU == ON)
-	err = mpu_standalone_init();
-	if(err) return err;
-	
-//	sys_barrier();
-	
-	err = mpu_test();
-	if(err) return err;
-#endif
-	
-	
-#if (TEST_AIOP == ON)
-	dpmng_set_dpaa_sys_ddr_region();
-
-	/* DPC process */
-	dpc_process();
-
-	/* LOG initialization */
-	log_init();
-
-	/* Initialize system */
-	err = sys_init();
-	if (err) {
-		dpmng_set_mc_status(MC_SYS_INIT_ERR);
-		return err;
-	}
-
-	/* decide if waiting for u-boot is needed before loading DPL*/
-	uboot_init_word = dpmng_uboot_init_message(); //TODO - check if it needs to be before sys_init?
-
-	is_master_core = sys_is_master_core();
-
-	if (is_master_core)
-	{
-		mc_get_version(&mc_version);
-		pr_info("MC Version:\nFirmware Major: "
-			"%d\nFirmware Minor: %d\nFirmware revision: %d\n",mc_version.major,
-			mc_version.minor, mc_version.revision);
-#ifdef DEBUG
-		mpu_print_tables();
-#endif
-	}
- 	if (is_master_core && ((err = global_init()) != 0)) {
-		return err;
-	}
-	sys_barrier();
-
-	if (uboot_init_word == UBOOT_INIT_MAGIC_WORD){
-		dpmng_set_mc_status(MC_INIT_DONE);
-		pr_info("MC waits for U-boot to finish initialization\n");
-		while(dpmng_uboot_init_message() && MC_INIT_DONE) ;
-	}
-
-	if (is_master_core ) {
-		pr_info("Processing layout\n");
-		err = dpl_process();
-		if (err)
-		{
-			dpmng_set_mc_status(MC_LO_PROCESS_ERR);
-			return err;
-		}
-	}
-	sys_barrier();
-
-	/* Load image to AIOP core#0;
-	 Read out-of-reset address from the provided image and load the image to DP-DDR */
-	if (is_master_core)
-		load_aiop();
-
-#endif
-
-
-
-//	sys_barrier();
 //
-//	if (is_master_core && ((err = global_post_init()) != 0)) {
-//		return err;
-//	}
-//	sys_barrier();
+//#if (STACK_OVERFLOW_DETECTION == 1)
+//    configure_stack_overflow_detection();
+//#endif
 //
-//	if (is_master_core)
-//		pr_info("Running applications\n");
+//    /* Initialize system */
+//    err = sys_init();
+//    if (err) {
+//    	cmgw_report_boot_failure();
+//        return err;
+//    }
+//    sys_barrier();
 //
-//	run_apps();
-//	sys_barrier();
+//    is_master_core = sys_is_master_core();
 //
-//	if (is_master_core)
-//	{
-//		pr_info("complete. Freeing resources and going out ...\n");
-//		global_free();
-//	}
+//    if(is_master_core) {
+//        if(cmgw_get_ntasks() > 2 /*4-tasks*/) {
+//        	pr_warn("More then 4 AIOP tasks/core.\n");
+//        }
+//    }
 //
-//	sys_barrier();
-//	/* TODO - complete - free everything here!!! */
+//    /* Only execute if core is a cluster master */
+//    if(sys_is_cluster_master())
+//    {
+//    	err = cluster_init();
+//    	if(err) {
+//    		cmgw_report_boot_failure();
+//    		return err;
+//    	}
+//    }
 //
-//	if (is_master_core)
-//		pr_info("MC status = 0x%08x.\n", dpmng_get_mc_status());
+//    if(is_master_core)
+//    {
+//    	err = tile_init();
+//    	if(err) {
+//    		cmgw_report_boot_failure();
+//    		return err;
+//    	}
 //
-//	/* Free system */
-//	sys_free();
-
-	return err;
+//    	err = global_early_init();
+//    	if(err) {
+//    	    cmgw_report_boot_failure();
+//    	    return err;
+//    	}
+//
+//    	err = apps_early_init();
+//    	if(err) {
+//    	    cmgw_report_boot_failure();
+//    	    return err;
+//    	}
+//
+//    	err = global_init();
+//    	if(err) {
+//    		cmgw_report_boot_failure();
+//    		return err;
+//    	}
+//    }
+//
+//    if(is_master_core)
+//    {
+//    	err = global_post_init();
+//    	if(err) {
+//    		cmgw_report_boot_failure();
+//    		return err;
+//    	}
+//
+//    	fsl_os_print("Running applications\n");
+//
+//    	err = run_apps();
+//    	if (err) {
+//    		cmgw_report_boot_failure();
+//    	    return err;
+//    	}
+//    }
+//
+//    core_ready_for_tasks();
+//
+//    if (is_master_core)
+//    	fsl_os_print("complete. freeing resources and going out ...\n");
+//    sys_barrier();
+//
+//    /* TODO - complete - free everything here!!! */
+//
+//    /* Free system */
+//    sys_free();
+//
+//    //TODO should never get here !!
+//    cmgw_report_boot_failure();
+    return err;
 }
-
--- tests/bringup/bringup_tests.h
+++ tests/bringup/bringup_tests.h
@@ -6,48 +6,11 @@
 #define OFF 0
 
 #define TEST_MEM_ACCESS 	OFF
-#define TEST_HW_SEM 		OFF
-#define TEST_SPINLOCK		OFF
-#define TEST_MPIC			OFF
-#define TEST_CONSOLE_PRINT	OFF
-#define TEST_MPU			OFF
-
-#define TEST_AIOP			OFF
-
 
 #if (TEST_MEM_ACCESS == ON)
 /* memory access test */
 int mem_standalone_init();
 int mem_test();
 #endif /* TEST_MEM_ACCESS */
-
-#if (TEST_HW_SEM == ON)
-int hw_sem_standalone_init();
-int hw_sem_test();
-#endif /* TEST_HW_SEM */
-
-#if (TEST_SPINLOCK == ON)
-int spinlock_standalone_init();
-int spinlock_test ();
-#endif /* TEST_SPINLOCK */
-
-#if (TEST_CONSOLE_PRINT == ON)
-int console_print_init();
-int console_print_test();
-#endif /* TEST_CONSOLE_PRINT */
-
-#if (TEST_MPIC == ON)
-int mpic_standalone_init();
-int mpic_test(); 
-#endif /* TEST_MPIC */
-
-#if (TEST_MPU == ON)
-int mpu_standalone_init();
-int mpu_test();
-#endif /* TEST_MPU */
-
-#if (TEST_AIOP == ON)
-//TODO
-#endif /* TEST_AIOP */
 
 #endif /* __BRINGUP_TESTS_H */
