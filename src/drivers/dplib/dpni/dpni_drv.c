3804b805157dfefae4c444724ad438196639d559
Alex Shraifel <alex.shraifel@freescale.com>
2015-01-21 09:01:44 +0200

    Add temp solution to set hard coded values for head room and tail room

--- src/drivers/dplib/dpni/dpni_drv.c
+++ src/drivers/dplib/dpni/dpni_drv.c
@@ -151,6 +151,7 @@ __COLD_CODE int dpni_drv_probe(struct mc_dprc *dprc,
 	uint16_t spid;
 	uint32_t sp_temp;
 	struct dpni_attr attributes;
+	struct dpni_buffer_layout layout = {0};
 	struct aiop_psram_entry *sp_addr;
 	struct aiop_psram_entry ddr_storage_profile;
 	struct aiop_ws_regs *wrks_addr = (struct aiop_ws_regs *)
@@ -204,6 +205,17 @@ __COLD_CODE int dpni_drv_probe(struct mc_dprc *dprc,
 			if ((err = dpni_set_pools(&dprc->io, dpni, &pools_params[DPNI_DRV_PEB_BPID_IDX])) != 0) {
 				pr_err("Failed to set the pools to DP-NI%d.\n", mc_niid);
 				return err;
+			}
+
+			/* TODO: This should be changed for dynamic solution. The hardcoded value is
+			 * temp solution.*/
+			layout.options =  DPNI_BUF_LAYOUT_OPT_DATA_HEAD_ROOM |
+						DPNI_BUF_LAYOUT_OPT_DATA_TAIL_ROOM;
+			layout.data_head_room = 96;
+			layout.data_tail_room = 52;
+			if ((err = dpni_set_rx_buffer_layout(&dprc->io, dpni, &layout)) != 0) {
+				pr_err("Failed to set rx buffer layout for DP-NI%d\n", mc_niid);
+				return -ENODEV;
 			}
 
 			/* Enable DPNI before updating the entry point function (EP_PC)
