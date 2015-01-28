8607a7457f1b3799e57e29b9bfd4618841d626f0
Alex Shraifel <alex.shraifel@freescale.com>
2015-01-28 17:41:08 +0200

    update led test to use same API as in MC

--- tests/bringup/leds.h
+++ tests/bringup/leds.h
@@ -13,6 +13,7 @@
 #define LED_CTL_OFFSET        0x04 /*led ctl is in offset 05*/
 #define LED_SWITCH_OFFSET     0x0C /*led switch is in offset 0x0E*/
 
+
 #define TURN_ON_LEDS_CTRL                                             \
 	{                                                             \
 		uint32_t *ptr = (uint32_t *)(FPGA_VIRT_ADDRESS +      \
@@ -67,5 +68,36 @@
 					LED_SWITCH_OFFSET);           \
 		*ptr ^= 0x00008000;                                   \
 	}
+/**
+ * @Function turn_led_configuration;
+ */
+static inline void turn_led_configuration(void)
+{
+	TURN_ON_LEDS_CTRL;
+}
+/**
+ * @Function turn_led_on;
+ * @param[in] led_num bitmask to choose which leds to turn on (8 bits = 8 leds)
+ */
+static inline void turn_led_on(uint8_t led_num)
+{
+	if(led_num | 0x01)
+		SWITCH_LED_1;
+	if(led_num | 0x02)
+		SWITCH_LED_2;
+	if(led_num | 0x04)
+		SWITCH_LED_3;
+	if(led_num | 0x08)
+		SWITCH_LED_4;
+	if(led_num | 0x10)
+		SWITCH_LED_5;
+	if(led_num | 0x20)
+		SWITCH_LED_6;
+	if(led_num | 0x40)
+		SWITCH_LED_7;
+	if(led_num | 0x80)
+		SWITCH_LED_8;
+}
+
 #endif /* __LED_H */
 
