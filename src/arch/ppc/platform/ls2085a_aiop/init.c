84e41fd520515d6f0f97b18782db0c90c1222b61
Alex Shraifel <alex.shraifel@freescale.com>
2015-02-10 14:39:58 +0200

    Hardcoded port 4 set for UART

--- src/arch/ppc/platform/ls2085a_aiop/init.c
+++ src/arch/ppc/platform/ls2085a_aiop/init.c
@@ -129,7 +129,7 @@ __COLD_CODE void fill_platform_parameters(struct platform_param *platform_param)
 	platform_param->clock_in_freq_khz = g_init_data.sl_info.sys_clk; //TODO check value
 	platform_param->l1_cache_mode = E_CACHE_MODE_INST_ONLY;
 	platform_param->console_type = PLTFRM_CONSOLE_DUART;
-	platform_param->console_id = (uint8_t)g_init_data.sl_info.uart_port_id;
+	platform_param->console_id = 4;//(uint8_t)g_init_data.sl_info.uart_port_id;
 
 	if(platform_param->clock_in_freq_khz == 0)
 	{
