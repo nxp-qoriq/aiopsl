7f8d5ec9c90d4126cba4141839d15210c92f2611
Ira Kalman <Ira.Kalman@freescale.com>
2015-02-12 10:43:40 +0200

    Removed hard coded UART - now set by DPC

--- src/arch/ppc/platform/ls2085a_aiop/init.c
+++ src/arch/ppc/platform/ls2085a_aiop/init.c
@@ -129,7 +129,11 @@ __COLD_CODE void fill_platform_parameters(struct platform_param *platform_param)
 	platform_param->clock_in_freq_khz = g_init_data.sl_info.sys_clk; //TODO check value
 	platform_param->l1_cache_mode = E_CACHE_MODE_INST_ONLY;
 	platform_param->console_type = PLTFRM_CONSOLE_DUART;
-	platform_param->console_id = 4;//(uint8_t)g_init_data.sl_info.uart_port_id;
+	platform_param->console_id = (uint8_t)g_init_data.sl_info.uart_port_id;
+	/* DPC UART ports are 1..4 */
+	if (platform_param->console_id == 0) {
+		platform_param->console_type = PLTFRM_CONSOLE_NONE;		
+	}
 
 	if(platform_param->clock_in_freq_khz == 0)
 	{
