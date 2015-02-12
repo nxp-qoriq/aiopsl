08da5e4e0d162121d00d3eeb7e851484d89c5d69
Yariv Sheffer <yariv.sheffer@freescale.com>
2015-02-12 09:09:16 +0200

    Added mutex testing to the simple_bu_test

--- build/aiop_sim/apps/simple_bu_test/src/simple_bu_test_yariv.c
+++ build/aiop_sim/apps/simple_bu_test/src/simple_bu_test_yariv.c
@@ -320,6 +320,63 @@ int simple_bu_yariv_test(void)
 		}
 		/* End of CDMA Reference Count Increment/Decrement Test */ 
 		
+		/* ************  CDMA Mutex Test ****************** */ 
+		
+		fsl_os_print("\nTesting CDMA Mutex\n");
+		
+		//uint8_t new_l2_dst[6] = {0x0a,0x0b,0x0c,0x0d,0x0e,0x0f};
+		//uint8_t new_l2_src[6] = {0x1a,0x1b,0x1c,0x1d,0x1e,0x1f};
+			
+		fsl_os_print("Doing CDMA cdma_mutex_lock_take (CDMA_MUTEX_WRITE_LOCK)\n");
+
+		cdma_mutex_lock_take(
+				cipher_key_addr, /* uint64_t mutex_id */
+				CDMA_MUTEX_WRITE_LOCK); /* uint32_t flags */
+		
+
+		fsl_os_print("Doing CDMA cdma_mutex_lock_release()\n");
+		cdma_mutex_lock_release(
+				cipher_key_addr);/* uint64_t mutex_id */
+		
+		
+		/* Lock and write external memory with CDMA */
+		fsl_os_print("Doing cdma_write_with_mutex(CDMA_PREDMA_MUTEX_WRITE_LOCK)\n");
+		cdma_write_with_mutex(
+				cipher_key_addr, /*uint64_t ext_address */
+				CDMA_PREDMA_MUTEX_WRITE_LOCK, /* uint32_t flags */
+				new_l2_dst, /* void *ws_src */
+				6); /* uint16_t size */
+		
+		/* Read and release */
+		fsl_os_print("Doing cdma_read_with_mutex(CDMA_POSTDMA_MUTEX_RM_BIT)\n");
+		cdma_read_with_mutex(
+				cipher_key_addr, /*uint64_t ext_address */
+				CDMA_POSTDMA_MUTEX_RM_BIT, /* uint32_t flags */
+				cipher_key_read, /* void *ws_dst */
+				6); /* uint16_t size */
+
+		/* Compare read data */
+		fsl_os_print("Comparing data from cdma_read_with_mutex()\n");
+		fsl_os_print("CDMA read = 0x");
+		for (i=0; i<6 ; i++) {
+				fsl_os_print("%x ",cipher_key_read[i]);
+				if (cipher_key_read[i] != new_l2_dst[i]) {
+					fsl_os_print("\nERROR: CDMA read mismatch\n");
+					err = 1;
+				}	
+		}
+		fsl_os_print("\n");
+
+		if (err)
+		{
+			fsl_os_print("ERROR: CDMA mutex test failed\n");
+		} else {
+			fsl_os_print("CDMA mutex test PASSED :-)\n");
+		}
+
+
+		/* End of CDMA Mutex Test */ 
+
 		fdma_discard_default_frame(FDMA_DIS_NO_FLAGS);
 	}
 	
