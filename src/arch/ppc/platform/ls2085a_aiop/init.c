df346415d636b2db567fc19dc8ad9dd92ca32398
Ira Kalman <Ira.Kalman@freescale.com>
2015-02-15 17:25:53 +0200

    Removed hard coded clock for uart

--- src/arch/ppc/platform/ls2085a_aiop/init.c
+++ src/arch/ppc/platform/ls2085a_aiop/init.c
@@ -126,7 +126,8 @@ __COLD_CODE void fill_platform_parameters(struct platform_param *platform_param)
 
 	memset(platform_param, 0, sizeof(platform_param));
 
-	platform_param->clock_in_freq_khz = g_init_data.sl_info.sys_clk; //TODO check value
+	/* Each UART is clocked by the platform clock/2 */
+	platform_param->clock_in_freq_khz = (g_init_data.sl_info.sys_clk / 2);
 	platform_param->l1_cache_mode = E_CACHE_MODE_INST_ONLY;
 	platform_param->console_type = PLTFRM_CONSOLE_DUART;
 	platform_param->console_id = (uint8_t)g_init_data.sl_info.uart_port_id;
