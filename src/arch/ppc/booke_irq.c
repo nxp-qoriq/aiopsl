d03a2381a8d4fba4143d5ff04bce920be2899669
Danny Shultz <danny.shultz@freescale.com>
2015-03-12 17:27:57 +0200

    initial stack exception handling

--- src/arch/ppc/booke_irq.c
+++ src/arch/ppc/booke_irq.c
@@ -87,6 +87,9 @@ void booke_generic_exception_isr(uint32_t intr_entry)
 		break;
 	case(0x10):
 		pr_debug("core %d int: MACHINE_CHECK\n", core_get_id());
+		if(booke_get_spr_MCSR() | 0x0800 /* STACK_ERR */) {
+			pr_debug("Stack exception\n");
+		}
 		break;
 	case(0x20):
 		pr_debug("core %d int: DATA_STORAGE\n", core_get_id());
@@ -213,32 +216,33 @@ asm static void branch_table(void) {
     /***************************************************/
     .align 0x100
 exception_irq:
-    li       r0, 0x00000000
-    /* disable exceptions and interrupts */
-    mtspr    DBSR, r0
-    mtspr    DBCR0, r0
-    mtspr    DBCR2, r0
-    /* disable debug and interrupts in MSR */
-    mtmsr    r0
-    isync
-//    /* update stack overflow detection to 1-task (0x8000) */ 
-//    se_bgeni r4,16
-//    mtspr    DAC2,r4
-    /* clear stack pointer */
-    li       rsp, 0x7ff0
+//    li       r0, 0x00000000
+//    /* disable exceptions and interrupts */
+//    mtspr    DBSR, r0
+//    mtspr    DBCR0, r0
+//    mtspr    DBCR2, r0
+//    /* disable debug and interrupts in MSR */
+//    mtmsr    r0
+//    isync
     /* branch to isr */
     lis      r4,booke_generic_exception_isr@h
     ori      r4,r4,booke_generic_exception_isr@l
     mtlr     r4
     blrl
-    se_illegal
     
     /***************************************************/
     /*** machine check *********************************/
     /***************************************************/
     .align 0x100
 machine_irq:
-    se_dnh
+	mfspr    r4, MCSR
+	ori      r4, r4, 0x0800 /* STACK_ERR */
+	cmpwi    r4, 0
+	bne      cont_ex
+    li rsp,  0x7ff0 /* clear stack pointer */
+cont_ex:
+    b        exception_irq
+    
 }
 
 __COLD_CODE asm void booke_init_interrupt_vector(void)
