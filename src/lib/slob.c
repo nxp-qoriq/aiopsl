dae69cc413ef144561c41a1584c80c676b549fce
Boris Alesker <BorisA@freescale.com>
2015-04-07 18:45:16 +0300

    Reduce KW cyclomatic complexity  to 20

--- src/lib/slob.c
+++ src/lib/slob.c
@@ -210,6 +210,41 @@ static t_free_block * create_free_block(uint64_t base, uint64_t size)
     return p_free_block;
 }
 
+/****************************************************************/
+static int insert_free_block(t_free_block **p_new_b,t_MM *p_MM,t_free_block *p_curr_b,uint64_t end,
+		                     uint64_t alignment,uint64_t align_base,t_free_block *p_prev_b,
+		                     int     i)
+{
+	 /* This is an old code line that assumes that size of an allocated memory is
+	 * multiply of alignment. Replaced this by a condition that a new block
+	 * is greater than the current alignment.
+	 * if ( !p_curr_b && ((((uint64_t)(end-base)) & ((uint64_t)(alignment-1))) == 0) )
+	 * */
+	if ( !p_curr_b &&  (end-align_base) >= alignment )
+	{
+		if ((*p_new_b = create_free_block(align_base, end-align_base)) == NULL)
+			return  -ENOMEM;
+		if (p_prev_b)
+			p_prev_b->p_next = *p_new_b;
+		else
+			p_MM->free_blocks[i] = *p_new_b;
+	}
+	return 0;
+}
+/****************************************************************/
+
+static void update_boundaries(uint64_t alignment,t_free_block *p_new_b,
+		                      t_free_block *p_curr_b,uint64_t end,uint64_t base)
+{
+    if ((alignment == 1) && !p_new_b)
+	{
+		if ( p_curr_b && base > p_curr_b->base )
+			base = p_curr_b->base;
+		if ( p_curr_b && end < p_curr_b->end )
+			end = p_curr_b->end;
+	}
+}
+
 /****************************************************************
  *  Routine:    AddFree
  *
@@ -316,30 +351,10 @@ static int add_free(t_MM *p_MM, uint64_t base, uint64_t end)
         /* If no free block found to be updated, insert a new free block
          * to the end of the free list.
          */
-        /* This is an old code line that assumes that size of an allocated memory is
-         * multiply of alignment. Replaced this by a condition that a new block
-         * is greater than the current alignment.
-         * if ( !p_curr_b && ((((uint64_t)(end-base)) & ((uint64_t)(alignment-1))) == 0) )
-         * */
-        if ( !p_curr_b &&  (end-align_base) >= alignment )
-        {
-            if ((p_new_b = create_free_block(align_base, end-align_base)) == NULL)
-                RETURN_ERROR(MAJOR, ENOMEM, NO_MSG);
-
-            if (p_prev_b)
-                p_prev_b->p_next = p_new_b;
-            else
-                p_MM->free_blocks[i] = p_new_b;
-        }
-
+        if(0 != insert_free_block(&p_new_b,p_MM,p_curr_b,end,alignment,align_base,p_prev_b,i))
+            RETURN_ERROR(MAJOR, ENOMEM, NO_MSG);
         /* Update boundaries of the new free block */
-        if ((alignment == 1) && !p_new_b)
-        {
-            if ( p_curr_b && base > p_curr_b->base )
-                base = p_curr_b->base;
-            if ( p_curr_b && end < p_curr_b->end )
-                end = p_curr_b->end;
-        }
+        update_boundaries(alignment,p_new_b,p_curr_b,end,base);
     }// for
 
     return (0);
