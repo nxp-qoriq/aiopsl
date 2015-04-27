bc8eb0138be368b6c0ed5c858842d1bc786489a3
Boris Alesker <BorisA@freescale.com>
2015-04-27 10:59:37 +0300

    Reorganization in slob for buffer_pool
    Change-Id: I4d37c75c4f64ae48a86e240d34062bb8e4ce59e2

--- src/lib/slob.h
+++ src/lib/slob.h
@@ -58,16 +58,17 @@ typedef struct t_mem_block {
 
 
 /* t_FreeBlock data structure defines parameters of the Free Block */
-typedef struct t_free_block
+typedef struct t_slob_block
 {
-    struct t_free_block *p_next;     /* Pointer to the next free block */
+    struct t_slob_block *p_next;     /* Pointer to the next free block */
 
     uint64_t  base;                 /* Base address of the block */
     uint64_t  end;                  /* End address of the block */
-} t_free_block;
+} t_slob_block;
 
 
 /* t_BusyBlock data structure defines parameters of the Busy Block  */
+#if 0
 typedef struct t_busy_block
 {
     struct t_busy_block *p_next;         /* Pointer to the next free block */
@@ -77,6 +78,7 @@ typedef struct t_busy_block
     char        name[MM_MAX_NAME_LEN];  /* That block of memory was allocated for
                                            something specified by the Name */
 } t_busy_block;
+#endif
 
 
 /* t_MM data structure defines parameters of the MM object */
@@ -85,12 +87,12 @@ typedef struct t_MM
 #ifdef AIOP
     uint8_t          *lock;
 #else
-    fsl_handle_t        lock;
+     struct spinlock    lock;
 #endif
 
     t_mem_block      *mem_blocks;     /* List of memory blocks (Memory list) */
-    t_busy_block     *busy_blocks;    /* List of busy blocks (Busy list) */
-    t_free_block     *free_blocks[MM_MAX_ALIGNMENT + 1];
+    t_slob_block     *busy_blocks;    /* List of busy blocks (Busy list) */
+    t_slob_block     *free_blocks[MM_MAX_ALIGNMENT + 1];
                                     /* Alignment lists of free blocks (Free lists) */
 
     uint64_t        free_mem_size;    /* Total size of free memory (in bytes) */
@@ -99,6 +101,7 @@ typedef struct t_MM
     uint64_t        base;
     uint64_t        size;
     fsl_handle_t    h_mem_mng; // boot memory manager
+    fsl_handle_t    h_slob_bf_pool;
 } t_MM;
 
 
