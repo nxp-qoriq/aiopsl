d404811b748eab2838a46ecba9d5dd2a3fab82c3
Hagit Gilboa <hagit.gilboa@freescale.com>
2015-04-29 09:56:34 +0300

    Add simple_bu_parser_test()

--- tests/aiop_core_lib/build/simple_bu_test/src/simple_bu_test.h
+++ tests/aiop_core_lib/build/simple_bu_test/src/simple_bu_test.h
@@ -1,6 +1,7 @@
 
 int parser_init(uint8_t *prpid);
 int simple_bu_hagit_test(void);
+int simple_bu_parser_test(void);
 int simple_bu_gal_test(void);
 int simple_bu_ilan_test(void);
 int simple_bu_yariv_test(void);
