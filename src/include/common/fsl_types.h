db72ebc24fbd9075c793a87c25f47da244460497
Alex Shraifel <alex.shraifel@freescale.com>
2015-07-23 11:58:02 +0300

    merge types_mw.h to fsl_types.h

--- src/include/common/fsl_types.h
+++ src/include/common/fsl_types.h
@@ -32,19 +32,49 @@
 #ifndef __FSL_TYPES_H
 #define __FSL_TYPES_H
 
-#if defined(__GNUC__) && defined(__cplusplus)
-#include "types_gpp.h"
+#include <stdint.h>
+#include <stddef.h>
 
-#elif defined(__GNUC__)
-#include "types_gcc.h"
+#define _prepacked
+#define _packed
 
-#elif defined(__MWERKS__)
-#include "types_mw.h"
+/* temporary, due to include issues */
+typedef uint32_t uintptr_t;
+typedef int32_t intptr_t;
 
-#else
-#error "missing types definition"
-#endif
 
+#ifndef NULL
+#define NULL ((0L))
+#endif /* NULL */
+
+#define STRINGTYPE(a) #a
+#define DEFINESECTION(x) section code_type x data_mode=far_abs code_mode=pc_rel
+#define PUSH _Pragma("push")
+#define STARTSECTION(s) _Pragma(STRINGTYPE(DEFINESECTION(s)))
+#define START_CODE_IN_SECTION(s) PUSH STARTSECTION(s)
+#define POP _Pragma("pop")
+/**declare function as entry point*/
+#define ENTRY_POINT   __declspec(entry_point)
+/** Task global variables area */
+#define __TASK __declspec(section ".tdata")
+
+/** Shared-SRAM code location */
+#pragma section RX ".stext_vle"
+#define __WARM_CODE __declspec(section ".stext_vle")
+#define __START_WARM_CODE START_CODE_IN_SECTION(".stext_vle")
+#define __END_WARM_CODE POP
+
+/** DDR code location */
+#pragma section RX ".dtext_vle"
+#define __COLD_CODE __declspec(section ".dtext_vle")
+#define __START_COLD_CODE START_CODE_IN_SECTION(".dtext_vle")
+#define __END_COLD_CODE POP
+
+/** i-RAM code location */
+#pragma section RX ".itext_vle"
+#define __HOT_CODE __declspec(section ".itext_vle")
+#define __START_HOT_CODE START_CODE_IN_SECTION(".itext_vle")
+#define __END_HOT_CODE POP
 
 /**************************************************************************//**
  @Description   General Handle
