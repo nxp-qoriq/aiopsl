c50e1cc0c5a574140231b03f694438079d8861c0
Chantal Berdah <chantal.berdah@freescale.com>
2015-08-05 15:24:32 +0300

    Remove unnecessary ifdef

--- src/drivers/netf/ipr/00_01/ipr.c
+++ src/drivers/netf/ipr/00_01/ipr.c
@@ -821,9 +821,6 @@ int ipr_miss_handling(struct ipr_instance *instance_params_ptr,
 	int sr_status;
 	uint16_t timeout_value;
 	uint8_t	 keysize;
-#ifdef REV2_RULEID
-	uint64_t rule_id;
-#endif
 	
 	/* Miss */
 	sr_status = cdma_acquire_context_memory(
@@ -864,11 +861,8 @@ int ipr_miss_handling(struct ipr_instance *instance_params_ptr,
 				TABLE_ACCEL_ID_CTLU,
 				instance_params_ptr->table_id_ipv4,
 				&rule,
-#ifdef REV2_RULEID
-				keysize,&rule_id);
-#else
 				keysize);
-#endif
+
 		if (sr_status == -ENOMEM) {
 			/* Maximum open reassembly is reached */
 			ipr_stats_update(
@@ -916,11 +910,8 @@ int ipr_miss_handling(struct ipr_instance *instance_params_ptr,
 			      TABLE_ACCEL_ID_CTLU,
 			      instance_params_ptr->table_id_ipv6,
 			      &rule,
-#ifdef REV2_RULEID
-				keysize,&rule_id);
-#else
 				keysize);
-#endif
+
 	    if (sr_status == -ENOMEM) {
 		    /* Maximum open reassembly is reached */
 		    ipr_stats_update(
