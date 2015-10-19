324f8f0477fd322e5ba47aecbc25ba45f29787c4
Ira Kalman <Ira.Kalman@freescale.com>
2015-10-19 13:43:52 +0300

    apps.h documentation is at app_process_packet

--- tests/roc_process_packet/apps.h
+++ tests/roc_process_packet/apps.h
@@ -28,6 +28,9 @@
 @File          apps.h
 
 @Description   This file contains the AIOP SL user defined setup.
+
+Please refer to the apps.h of app_process_packet for the full documentation
+of all the macros.
 *//***************************************************************************/
 
 #ifndef __APPS_H
@@ -35,85 +38,26 @@
 
 #include "apps_arch.h"
 
-/**************************************************************************//**
-@Group		app_init	General
 
-@Description	Use the following macros to define the general AIOP SL setup.
-@{
-*//***************************************************************************/
-
-#define APP_INIT_TASKS_PER_CORE		4	/**< Tasks per core in AIOP */
+#define APP_INIT_TASKS_PER_CORE		4
 #define APP_INIT_APP_MAX_NUM		10
-/**<  Maximal number of applications to be filled by build_apps_array() */
 
-/** @} */ /* end of app_init */
+#define APP_MEM_DP_DDR_SIZE	ARCH_DP_DDR_SIZE
+#define APP_MEM_PEB_SIZE	(512 * KILOBYTE)
+#define APP_MEM_SYS_DDR1_SIZE	ARCH_SYS_DDR_SIZE
 
-/**************************************************************************//**
-@Group		app_mem		Memory Allocation
-
-@Description	Use the following macros to define the memory sizes required by
-		the sum of all AIOP applications and Service Layer.
-		All the sizes refer to the sum of all static (LCF) and
-		dynamic (malloc) allocations.
-		All the sizes should be aligned to a power of 2.
-@{
-*//***************************************************************************/
-#define APP_MEM_DP_DDR_SIZE	ARCH_DP_DDR_SIZE/**< DP DDR size */
-#define APP_MEM_PEB_SIZE	(512 * KILOBYTE)/**< PEB size */
-#define APP_MEM_SYS_DDR1_SIZE 	(64 * MEGABYTE)	/**< System DDR size */
-
-/** @} */ /* end of app_mem */
-
-/**************************************************************************//**
-@Group		app_ctlu	CTLU
-
-@Description	Use the following macros to define the CTLU setup.
-		All the sizes should be aligned to a power of 2.
-@{
-*//***************************************************************************/
-
-#define APP_CTLU_SYS_DDR_NUM_ENTRIES	2048 /**< SYS DDR number of entries */
+#define APP_CTLU_SYS_DDR_NUM_ENTRIES	2048
 #define APP_CTLU_DP_DDR_NUM_ENTRIES	ARCH_CTLU_DP_DDR_NUM_ENTRIES
-                                         /**< DP DDR number of entries */
-#define APP_CTLU_PEB_NUM_ENTRIES	2048 /**< PEB number of entries */
+#define APP_CTLU_PEB_NUM_ENTRIES	2048
 
-/** @} */ /* end of app_ctlu */
-
-/**************************************************************************//**
-@Group		app_mflu	MFLU
-
-@Description	Use the following macros to define the MFLU setup.
-		All the sizes should be aligned to a power of 2.
-@{
-*//***************************************************************************/
-
-#define APP_MFLU_SYS_DDR_NUM_ENTRIES	2048 /**< SYS DDR number of entries */
+#define APP_MFLU_SYS_DDR_NUM_ENTRIES	2048
 #define APP_MFLU_DP_DDR_NUM_ENTRIES	ARCH_MFLU_DP_DDR_NUM_ENTRIES
-                                        /**< DP DDR number of entries */
-#define APP_MFLU_PEB_NUM_ENTRIES	2048 /**< PEB number of entries */
+#define APP_MFLU_PEB_NUM_ENTRIES	2048
 
-/** @} */ /* end of app_mflu */
-
-/**************************************************************************//**
-@Group		app_dpni	DPNI
-
-@Description	Use the following macros to define the DPNI driver.
-		Two AIOP buffer pools are reserved for frame data buffers of all
-		AIOP DPNIs; One for frame data residing in DP-DDR and one for
-		frame data residing in PEB. The buffer amount, size and
-		alignment defined below applies to both pools and to all AIOP
-		DPNIs. Each DPNI uses two Storage Profile IDs (one for DP-DDR
-		and one for PEB) and therefore APP_DPNI_SPID_COUNT must
-		accommodate at least two SPIDs per AIOP DPNI.
-@{
-*//***************************************************************************/
-
-#define APP_DPNI_NUM_BUFS_IN_POOL	50	/**< Number of buffers */
-#define APP_DPNI_BUF_SIZE_IN_POOL	2048	/**< Size of buffer */
-#define APP_DPNI_BUF_ALIGN_IN_POOL	64	/**< Alignment of buffer */
-#define APP_DPNI_SPID_COUNT		8	/**< Max number of SPIDs */
-
-/** @} */ /* end of app_dpni */
+#define APP_DPNI_NUM_BUFS_IN_POOL	50
+#define APP_DPNI_BUF_SIZE_IN_POOL	2048
+#define APP_DPNI_BUF_ALIGN_IN_POOL	64
+#define APP_DPNI_SPID_COUNT		8
 
 #endif /* __APPS_H */
 
