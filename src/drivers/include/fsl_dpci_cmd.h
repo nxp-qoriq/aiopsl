5603d18d99ef38f3a1012d1b13ab81fdc11f0162
Daniela Ferber <daniela.ferber@freescale.com>
2015-10-19 08:29:23 +0300

    aligned Flibs with arena develop

--- src/drivers/include/fsl_dpci_cmd.h
+++ src/drivers/include/fsl_dpci_cmd.h
@@ -128,9 +128,11 @@ do { \
 	MC_RSP_OP(cmd, 0, 0,  32, uint32_t, mask)
 
 /*                cmd, param, offset, width, type, arg_name */
-#define DPCI_CMD_GET_IRQ_STATUS(cmd, irq_index) \
-	MC_CMD_OP(cmd, 0, 32, 8,  uint8_t,  irq_index)
-
+#define DPCI_CMD_GET_IRQ_STATUS(cmd, irq_index, status) \
+do { \
+	MC_CMD_OP(cmd, 0, 0,  32, uint32_t, status);\
+	MC_CMD_OP(cmd, 0, 32, 8,  uint8_t,  irq_index);\
+} while (0)
 /*                cmd, param, offset, width, type, arg_name */
 #define DPCI_RSP_GET_IRQ_STATUS(cmd, status) \
 	MC_RSP_OP(cmd, 0, 0,  32, uint32_t, status)
