694a4eadc520a68ec429a2368f8b8b972e7be283
Hagit Gilboa <hagit.gilboa@freescale.com>
2015-11-24 10:26:39 +0200

    Fixed ENGR369992

--- src/drivers/accel/parser/02_01/parser_inline.h
+++ src/drivers/accel/parser/02_01/parser_inline.h
@@ -95,14 +95,17 @@ inline int parse_result_generate(enum parser_starting_hxs_code starting_hxs,
 	__e_hwacceli(CTLU_PARSE_CLASSIFY_ACCEL_ID);
 
 	status = *(int32_t *)HWC_ACC_OUT_ADDRESS;
-	if (status == PARSER_HW_STATUS_SUCCESS) {
-		return 0;
-	} else if ((status ==
-			PARSER_HW_STATUS_L3_CHECKSUM_VALIDATION_SUCCEEDED) ||
-		(status ==
-			PARSER_HW_STATUS_L4_CHECKSUM_VALIDATION_SUCCEEDED) ||
-		(status ==
-			PARSER_HW_STATUS_L3_L4_CHECKSUM_VALIDATION_SUCCEEDED)) {
+	 if ((flags == PARSER_NO_FLAGS &&
+	((status & ~PARSER_HW_STATUS_L3_L4_CHECKSUM_GENERATION_SUCCEEDED)
+			== PARSER_HW_STATUS_SUCCESS))
+	|| (flags == PARSER_VALIDATE_L3_L4_CHECKSUM && 
+	status == PARSER_HW_STATUS_L3_L4_CHECKSUM_VALIDATION_SUCCEEDED) 
+	|| (flags == PARSER_VALIDATE_L3_CHECKSUM && 
+	((status & ~PARSER_HW_STATUS_L4_CHECKSUM_GENERATION_SUCCEEDED) ==
+		PARSER_HW_STATUS_L3_CHECKSUM_VALIDATION_SUCCEEDED))
+	|| (flags == PARSER_VALIDATE_L4_CHECKSUM && 
+	((status & ~PARSER_HW_STATUS_L3_CHECKSUM_GENERATION_SUCCEEDED) ==
+		PARSER_HW_STATUS_L4_CHECKSUM_VALIDATION_SUCCEEDED))){
 		return 0;
 	} else if (status & PARSER_HW_STATUS_CYCLE_LIMIT_EXCCEEDED) {
 		parser_exception_handler(PARSE_RESULT_GENERATE,
@@ -174,14 +177,17 @@ inline int parse_result_generate_default(uint8_t flags)
 	status = *(int32_t *)HWC_ACC_OUT_ADDRESS;
 	/* implementation of errors is priority based (if-else) since multiple
 	 * error indications may appear at the same time */
-	if (status == PARSER_HW_STATUS_SUCCESS) {
-		return 0;
-	} else if ((status ==
-			PARSER_HW_STATUS_L3_CHECKSUM_VALIDATION_SUCCEEDED) ||
-		(status ==
-			PARSER_HW_STATUS_L4_CHECKSUM_VALIDATION_SUCCEEDED) ||
-		(status ==
-			PARSER_HW_STATUS_L3_L4_CHECKSUM_VALIDATION_SUCCEEDED)) {
+	 if ((flags == PARSER_NO_FLAGS &&
+	((status & ~PARSER_HW_STATUS_L3_L4_CHECKSUM_GENERATION_SUCCEEDED)
+		== PARSER_HW_STATUS_SUCCESS))
+	|| (flags == PARSER_VALIDATE_L3_L4_CHECKSUM && 
+	status == PARSER_HW_STATUS_L3_L4_CHECKSUM_VALIDATION_SUCCEEDED) 
+	|| (flags == PARSER_VALIDATE_L3_CHECKSUM && 
+	((status & ~PARSER_HW_STATUS_L4_CHECKSUM_GENERATION_SUCCEEDED) ==
+		PARSER_HW_STATUS_L3_CHECKSUM_VALIDATION_SUCCEEDED))
+	|| (flags == PARSER_VALIDATE_L4_CHECKSUM && 
+	((status & ~PARSER_HW_STATUS_L3_CHECKSUM_GENERATION_SUCCEEDED) ==
+		PARSER_HW_STATUS_L4_CHECKSUM_VALIDATION_SUCCEEDED))){
 		return 0;
 	} else if (status & PARSER_HW_STATUS_CYCLE_LIMIT_EXCCEEDED) {
 		parser_exception_handler(PARSE_RESULT_GENERATE_DEFAULT,
@@ -226,7 +232,9 @@ inline int parse_result_generate_basic(void)
 
 
 	status = *(int32_t *)HWC_ACC_OUT_ADDRESS;
-       if (status == PARSER_HW_STATUS_SUCCESS) {
+      
+	if ((status & ~PARSER_HW_STATUS_L3_L4_CHECKSUM_GENERATION_SUCCEEDED)
+			== PARSER_HW_STATUS_SUCCESS) {
              return 0;
        } else if (status & PARSER_HW_STATUS_CYCLE_LIMIT_EXCCEEDED) {
 		parser_exception_handler(PARSE_RESULT_GENERATE_BASIC,
