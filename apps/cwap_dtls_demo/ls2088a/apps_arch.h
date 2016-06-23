62c09d1957cf49e723939e278440409a7cc6e38b
Horia Geantă <horia.geanta@nxp.com>
2016-06-23 20:02:57 +0300

    cwap/dtls: add cwap_dtls_demo application
    Signed-off-by: Sebastian Irimia <sebastian.irimia@nxp.com>
    Signed-off-by: Horia Geantă <horia.geanta@nxp.com>

---
+++ apps/cwap_dtls_demo/ls2088a/apps_arch.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2014-2015 Freescale Semiconductor, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of Freescale Semiconductor nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Freescale Semiconductor ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL Freescale Semiconductor BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**************************************************************************//**
+@File		apps_arch.h
+
+@Description	This file contains the application dp-ddr memory sizes.
+
+*//***************************************************************************/
+
+#ifndef __APPS_ARCH_H
+#define __APPS_ARCH_H
+
+#define ARCH_DP_DDR_SIZE	(128 * MEGABYTE)
+#define ARCH_CTLU_DP_DDR_NUM_ENTRIES (2048)
+#define ARCH_MFLU_DP_DDR_NUM_ENTRIES (2048)
+
+#define IPSEC_DEMO_MEM_ID MEM_PART_DP_DDR
+
+#endif /* __APPS_ARCH_H */
