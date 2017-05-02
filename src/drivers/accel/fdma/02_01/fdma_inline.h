6a46c6192bef2609562628d8e8d435738cf67c68
Grigore Ion <ion.grigore@freescale.com>
2017-05-02 16:49:31 +0300

    fdma : Split command error when SM is 1 and PSA is 2
    If PSA = 2, after the split command is executed, the split fragment
    is closed. There is no frame handle #2 in the result.
    When SM = 1, the API function should update the returned split_size_sf
    parameter only if the fragment is not closed.
    
    Signed-off-by: Grigore Ion <ion.grigore@freescale.com>

--- src/drivers/accel/fdma/02_01/fdma_inline.h
+++ src/drivers/accel/fdma/02_01/fdma_inline.h
@@ -817,7 +817,7 @@ inline int fdma_split_frame(
 	/* command parameters and results */
 	uint32_t arg1, arg2, arg3, arg4;
 	uint32_t split_frame_length;
-	uint32_t res1_sm, res2_sm;/*restore results in case of split by SF bit*/
+	uint32_t res2_sm; /* restore result in case of split by SF bit */
 	int8_t  res1;
 
 	/* prepare command parameters */
@@ -847,18 +847,15 @@ inline int fdma_split_frame(
 			(HWC_ACC_OUT_ADDRESS2 + FDMA_SEG_HANDLE_OFFSET));
 		if (!(params->flags & FDMA_SPLIT_PSA_CLOSE_FRAME_BIT)) {
 			if (params->flags & FDMA_SPLIT_SM_BIT) { 
-				/* save results of split command */
-				res1_sm = *((uint32_t *)HWC_ACC_OUT_ADDRESS);
+				/* save result of split command */
 				res2_sm = *((uint32_t *)HWC_ACC_OUT_ADDRESS2);
 				get_frame_length(params->split_frame_handle, 
 						&(split_frame_length));
 				params->split_size_sf = 
 						(uint16_t)split_frame_length;
-				/* restore results of split command */
-				*((uint32_t *)HWC_ACC_OUT_ADDRESS) = res1_sm;
+				/* restore result of split command */
 				*((uint32_t *)HWC_ACC_OUT_ADDRESS2) = res2_sm;
 			}
-			//LDPAA_FD_SET_SL(HWC_FD_ADDRESS, 0);
 			LDPAA_FD_SET_LENGTH((uint32_t)params->fd_dst,
 					params->split_size_sf);
 		}
@@ -889,16 +886,15 @@ inline int fdma_split_frame(
 		
 		if ((((uint32_t)params->fd_dst) != HWC_FD_ADDRESS) && 
 		    (params->source_frame_handle ==  PRC_GET_FRAME_HANDLE())) {
-			if (params->flags & FDMA_SPLIT_SM_BIT) {
-				/* save results of split command */
-				res1_sm = *((uint32_t *)HWC_ACC_OUT_ADDRESS);
+			if (params->flags & FDMA_SPLIT_SM_BIT &&
+			    !(params->flags & FDMA_SPLIT_PSA_CLOSE_FRAME_BIT)) {
+				/* save result of split command */
 				res2_sm = *((uint32_t *)HWC_ACC_OUT_ADDRESS2);
 				get_frame_length(params->split_frame_handle, 
 						&(split_frame_length));
 				params->split_size_sf = 
 						(uint16_t)split_frame_length;
-				/* restore results of split command */
-				*((uint32_t *)HWC_ACC_OUT_ADDRESS) = res1_sm;
+				/* restore result of split command */
 				*((uint32_t *)HWC_ACC_OUT_ADDRESS2) = res2_sm;
 			}
 			LDPAA_FD_UPDATE_LENGTH(HWC_FD_ADDRESS, 0, 
