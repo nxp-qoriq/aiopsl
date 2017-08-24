a0531c522c297c88799cad043319e59bc8ca8179
George Stefan <George.Stefan@nxp.com>
2017-08-24 11:26:13 +0300

    firmware no.: 8.1.0
    Increment to 8.1.0
    
    Signed-off-by: George Stefan <George.Stefan@nxp.com>

--- src/kernel/include/fsl_sys.h
+++ src/kernel/include/fsl_sys.h
@@ -50,8 +50,8 @@
 
 /* Same version for all the devices */
 #define SYS_REV_MAJOR	8
-#define SYS_REV_MINOR	0
-#define SYS_REVISION	3
+#define SYS_REV_MINOR	1
+#define SYS_REVISION	0
 
 /*!
  * @ingroup aiopapp_init
